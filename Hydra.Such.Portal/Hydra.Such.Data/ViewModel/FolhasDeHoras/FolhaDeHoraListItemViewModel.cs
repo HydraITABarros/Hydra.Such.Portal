48e4131c6a30f9e3c4bfd09f8f383b3626ae4289
Amaro Romão <ARomao@such.pt>
2017-10-17 12:05:22 +0100

    Folhas de Horas. Amaro Romão

--- Hydra.Such.Portal/Hydra.Such.Data/ViewModel/FolhasDeHoras/FolhaDeHoraListItemViewModel.cs
+++ Hydra.Such.Portal/Hydra.Such.Data/ViewModel/FolhasDeHoras/FolhaDeHoraListItemViewModel.cs
@@ -8,21 +8,29 @@ namespace Hydra.Such.Data.ViewModel.FolhasDeHoras
     {
         public string FolhaDeHorasNo { get; set; }
         public int? Area { get; set; }
+        public string AreaText { get; set; }
         public string ProjectNo { get; set; }
         public string EmployeeNo { get; set; }
         public DateTime? DateDepartureTime { get; set; }
+        public string DateDepartureTimeText { get; set; }
         public DateTime? DateTimeArrival { get; set; }
+        public string DateTimeArrivalText { get; set; }
         public int? TypeDeslocation { get; set; }
+        public string TypeDeslocationText { get; set; }
         public string CodeTypeKms { get; set; }
         public bool? DisplacementOutsideCity { get; set; }
+        public string DisplacementOutsideCityText { get; set; }
         public string Validators { get; set; }
         public int? Status { get; set; }
-        public string StatusDescription { get; set; }
+        public string StatusText { get; set; }
         public string CreatedBy { get; set; }
         public DateTime? DateTimeCreation { get; set; }
+        public string DateTimeCreationText { get; set; }
         public DateTime? DateTimeLastState { get; set; }
+        public string DateTimeLastStateText { get; set; }
         public string UserCreation { get; set; }
         public DateTime? DateTimeModification { get; set; }
+        public string DateTimeModificationText { get; set; }
         public string UserModification { get; set; }
     }
 }
