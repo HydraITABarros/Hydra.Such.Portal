5af9c44663ed011cb6fc144ff3c596379e1913d5
Eduardo Lopes <eduardojorgelopes@hotmail.com>
2017-10-19 14:31:50 +0100

    Alterações nos elementos: comboboxs, textbox, grid...

--- Hydra.Such.Portal/Hydra.Such.Portal/wwwroot/lib/vaadin-combo-box/wct.conf.js
+++ Hydra.Such.Portal/Hydra.Such.Portal/wwwroot/lib/vaadin-combo-box/wct.conf.js
@@ -6,23 +6,21 @@ module.exports = {
   },
   registerHooks: function(context) {
     var saucelabsPlatforms = [
-      'OS X 10.11/iphone@9.3',
-      'OS X 10.11/ipad@9.3',
-      // 'OS X 10.11/iphone@10.2', // iOS 10 tests need to be run manually
-      // 'OS X 10.11/ipad@10.2',   // until we figure out why they timeout in Sauce Labs.
-      'Windows 10/microsoftedge@14',
+      'macOS 10.12/iphone@10.3',
+      'macOS 10.12/ipad@10.3',
+      'Windows 10/microsoftedge@15',
       'Windows 10/internet explorer@11',
-      'OS X 10.11/safari@10.0'
+      'macOS 10.12/safari@10.0'
     ];
 
     var cronPlatforms = [
-      'Windows 10/chrome@55',
-      'Windows 10/firefox@50'
+      'Android/chrome',
+      'Windows 10/chrome@59',
+      'Windows 10/firefox@54'
     ];
 
     if (argv.env === 'saucelabs') {
       context.options.plugins.sauce.browsers = saucelabsPlatforms;
-
     } else if (argv.env === 'saucelabs-cron') {
       context.options.plugins.sauce.browsers = cronPlatforms;
     }
