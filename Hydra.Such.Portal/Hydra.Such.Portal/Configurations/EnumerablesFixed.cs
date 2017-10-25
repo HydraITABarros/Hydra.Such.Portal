92359ec61646bb6f51fd00428d0dba5f0e3dc33b
Amaro Romão <ARomao@such.pt>
2017-10-25 13:56:05 +0100

    Folha de Horas - 25/10/2017 - Amaro Romão

--- Hydra.Such.Portal/Hydra.Such.Portal/Configurations/EnumerablesFixed.cs
+++ Hydra.Such.Portal/Hydra.Such.Portal/Configurations/EnumerablesFixed.cs
@@ -683,21 +683,21 @@ namespace Hydra.Such.Portal.Configurations
             }
         };
 
-        public static readonly List<EnumData> FolhaDeHoraCodeTypeKms = new List<EnumData>()
+        public static readonly List<EnumDataString> FolhaDeHoraCodeTypeKms = new List<EnumDataString>()
         {
-            new EnumData()
+            new EnumDataString()
             {
-                Id = 0,
+                Id = "KM",
                 Value = "Kilometros"
             },
-            new EnumData()
+            new EnumDataString()
             {
-                Id = 1,
+                Id = "KMC",
                 Value = "Kilometros (Coimbra)"
             },
-            new EnumData()
+            new EnumDataString()
             {
-                Id = 2,
+                Id = "KMP",
                 Value = "Kilometros (Porto)"
             }
         };
@@ -916,6 +916,12 @@ namespace Hydra.Such.Portal.Configurations
     public class EnumData
     {
         public int Id { get; set; }
+        public string Value { get; set; }
+    }
+
+    public class EnumDataString
+    {
+        public string Id { get; set; }
         public string Value { get; set; }
     }
 }
