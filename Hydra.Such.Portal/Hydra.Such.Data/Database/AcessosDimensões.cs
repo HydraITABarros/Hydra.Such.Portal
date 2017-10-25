5dc2c1153daf691aa8ac044c53d53d1bed7ba8e4
JPereira <jpereira@hydra.pt>
2017-10-25 11:26:41 +0100

    Adicionadas dimensões utilizadores

---
+++ Hydra.Such.Portal/Hydra.Such.Data/Database/AcessosDimensões.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Hydra.Such.Data.Database
+{
+    public class AcessosDimensões
+    {
+        public string IdUtilizador { get; set; }
+        public int Dimensão { get; set; }
+        public string ValorDimensão { get; set; }
+    }
+}
