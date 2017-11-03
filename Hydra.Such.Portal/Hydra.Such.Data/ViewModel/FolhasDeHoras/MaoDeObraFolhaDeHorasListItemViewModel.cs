fa39a41b1390dcb27c23767ec265227a80746759
Amaro Romão <ARomao@such.pt>
2017-11-03 10:31:16 +0000

    Folha de Horas - 03/11/2017 - Amaro Romão

---
+++ Hydra.Such.Portal/Hydra.Such.Data/ViewModel/FolhasDeHoras/MaoDeObraFolhaDeHorasListItemViewModel.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+
+namespace Hydra.Such.Data.ViewModel.FolhasDeHoras
+{
+    public class MaoDeObraFolhaDeHorasListItemViewModel
+    {
+        public string FolhaDeHorasNo { get; set; }
+        public int? LineNo { get; set; }
+        public DateTime? Date { get; set; }
+        public string EmployedNo { get; set; }
+        public string ProjectNo { get; set; }
+        public int? WorkTypeCode { get; set; }
+        public DateTime? StartTime { get; set; }
+        public String StartTimeText { get; set; }
+        public DateTime? EndTime { get; set; }
+        public string EndTimeText { get; set; }
+        public Boolean? LunchTime { get; set; }
+        public Boolean? DinnerTime { get; set; }
+        public string FamilyCodeResource { get; set; }
+        public string ResourceNo { get; set; }
+        public string UnitCodeMeasure { get; set; }
+        public int? OMTypeCode { get; set; }
+        public DateTime? HoursNo { get; set; }
+        public string HoursNoText { get; set; }
+        public decimal DirectUnitCost { get; set; }
+        public decimal CostPrice { get; set; }
+        public decimal SalePrice { get; set; }
+        public decimal TotalPrice { get; set; }
+        public DateTime? DateTimeCreation { get; set; }
+        public string UserCreation { get; set; }
+        public DateTime? DateTimeModification { get; set; }
+        public string UserModification { get; set; }
+    }
+}
--- Hydra.Such.Portal/Hydra.Such.Portal/Controllers/PopulateDropdownsController.cs
+++ Hydra.Such.Portal/Hydra.Such.Portal/Controllers/PopulateDropdownsController.cs
@@ -14,6 +14,7 @@ using Hydra.Such.Data.ViewModel.ProjectDiary;
 using Microsoft.AspNetCore.Authorization;
 using Hydra.Such.Data.ViewModel;
 using Hydra.Such.Data.Logic.Contracts;
+using Hydra.Such.Data.Logic.FolhaDeHora;
 
 namespace Hydra.Such.Portal.Controllers
 {
@@ -132,10 +133,10 @@ namespace Hydra.Such.Portal.Controllers
         [HttpPost]
         public JsonResult GetFolhaDeHoraPercursoOrigemDestino()
         {
-            List<DDMessageString> result = DBNAV2017PaymentTerms.GetAll(_config.NAVDatabaseName, _config.NAVCompanyName, "").Select(x => new DDMessageString()
+            List<DDMessageString> result = DBOrigemDestinoFh.GetAll().Select(x => new DDMessageString()
             {
-                id = x.Code,
-                value = x.Description
+                id = x.Código,
+                value = x.Descrição
             }).ToList();
             return Json(result);
         }
