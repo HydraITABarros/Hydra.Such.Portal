33d2ae7c1ce0e0e4a04cdcbaa52aca79e52e9126
André Bastos <andre.bastos@geratriz.pt>
2017-11-22 18:31:58 +0000

    Fix hide duplicate properties on configUtilizadores - after merge

--- Hydra.Such.Portal/Hydra.Such.Data/Database/SuchDBContext.cs
+++ Hydra.Such.Portal/Hydra.Such.Data/Database/SuchDBContext.cs
@@ -936,11 +936,11 @@ namespace Hydra.Such.Data.Database
                 entity.Property(e => e.ProcedimentosEmailEnvioParaArea).HasMaxLength(50);
 
                 entity.Property(e => e.ProcedimentosEmailEnvioParaArea2).HasMaxLength(50);
-
+                /*
                 entity.Property(e => e.ProcedimentosEmailEnvioParaCa)
                     .HasColumnName("ProcedimentosEmailEnvioParaCA")
                     .HasMaxLength(50);
-
+                */
                 entity.Property(e => e.UtilizadorCriação)
                     .HasColumnName("Utilizador Criação")
                     .HasMaxLength(50);
