644ba06e47fb345c517e8eb082b3c46bb07dc24b
Amaro Romão <ARomao@such.pt>
2017-11-23 09:13:29 +0000

    Folha de Horas - 23/11/2017 - Amaro Romão

---
+++ Hydra.Such.Portal/Hydra.Such.Data/Logic/FolhaDeHora/DBAutorizacaoFHRH.cs
@@ -0,0 +1,87 @@
+﻿using Hydra.Such.Data.Database;
+using Hydra.Such.Data.ViewModel.FH;
+using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Linq;
+using System.Text;
+
+namespace Hydra.Such.Data.Logic.FolhaDeHora
+{
+    public class DBAutorizacaoFHRH
+    {
+        public static List<AutorizacaoFHRH> GetAll()
+        {
+            try
+            {
+                using (var ctx = new SuchDBContext())
+                {
+                    return ctx.AutorizacaoFHRH.ToList();
+                }
+            }
+            catch (Exception e)
+            {
+                return null;
+            }
+        }
+
+        public static AutorizacaoFHRH Create(AutorizacaoFHRH ObjectToCreate)
+        {
+            try
+            {
+                using (var ctx = new SuchDBContext())
+                {
+                    ObjectToCreate.DataHoraCriacao = DateTime.Now;
+                    ctx.AutorizacaoFHRH.Add(ObjectToCreate);
+                    ctx.SaveChanges();
+                }
+
+                return ObjectToCreate;
+            }
+            catch (Exception e)
+            {
+
+                return null;
+            }
+        }
+
+        public static AutorizacaoFHRH Update(AutorizacaoFHRH ObjectToUpdate)
+        {
+            try
+            {
+                using (var ctx = new SuchDBContext())
+                {
+                    ObjectToUpdate.DataHoraModificacao = DateTime.Now;
+                    ctx.AutorizacaoFHRH.Update(ObjectToUpdate);
+                    ctx.SaveChanges();
+                }
+
+                return ObjectToUpdate;
+            }
+            catch (Exception e)
+            {
+
+                return null;
+            }
+        }
+
+        public static bool Delete(AutorizacaoFHRH ObjectToDelete)
+        {
+            try
+            {
+                using (var ctx = new SuchDBContext())
+                {
+                    ctx.AutorizacaoFHRH.Remove(ObjectToDelete);
+                    ctx.SaveChanges();
+                }
+
+                return true;
+            }
+            catch (Exception e)
+            {
+
+                return false;
+            }
+        }
+    }
+}
