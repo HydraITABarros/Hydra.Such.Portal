2e7639c1fdd5a93228c91a9e736d058113348785
Daniel Alves <dribeiro@hydra.pt>
2017-12-04 10:31:19 +0000

    DR

---
+++ Hydra.Such.Portal/Hydra.Such.Portal/Areas/Compras/Controllers/PreRequisicoesController.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Hydra.Such.Portal.Areas.Compras.Controllers
+{
+    public class PreRequisicoesController : Controller
+    {
+        [Area("Compras")]
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
