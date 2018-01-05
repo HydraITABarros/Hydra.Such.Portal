94be499b5032135a0ae6f549526cc6132e4a6f26
João Pereira <jpereira@hydra.pt>
2018-01-05 14:50:48 +0000

    .

--- Hydra.Such.Portal/Hydra.Such.Data/NAV/NAVPurchaseHeaderService.cs
+++ Hydra.Such.Portal/Hydra.Such.Data/NAV/NAVPurchaseHeaderService.cs
@@ -45,15 +45,14 @@ namespace Hydra.Such.Data.NAV
             ws_Client.ClientCredentials.Windows.AllowedImpersonationLevel = System.Security.Principal.TokenImpersonationLevel.Delegation;
             ws_Client.ClientCredentials.Windows.ClientCredential = new NetworkCredential(WSConfigurations.WS_User_Login, WSConfigurations.WS_User_Password, WSConfigurations.WS_User_Domain);
 
-            try
-            {
-                WSPurchaseInvHeader.Create_Result result = await ws_Client.CreateAsync(navCreate);
-                return result;
-            }
-            catch (Exception ex)
-            {
-                throw;
-            }
+            //try
+            //{
+                return await ws_Client.CreateAsync(navCreate);
+            //}
+            //catch (Exception ex)
+            //{
+            //    throw;
+            //}
         }
     }
 }
