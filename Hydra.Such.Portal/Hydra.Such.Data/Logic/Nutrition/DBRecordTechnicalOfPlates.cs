9afe4577073419d0bf8bcf039213d7188cc50f9d
Eduardo Lopes <eduardojorgelopes@hotmail.com>
2018-01-09 16:11:38 +0000

    ficha tecnica de pratos

---
+++ Hydra.Such.Portal/Hydra.Such.Data/Logic/Nutrition/DBRecordTechnicalOfPlates.cs
@@ -0,0 +1,150 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using Hydra.Such.Data.Database;
+using Hydra.Such.Data.ViewModel.Nutrition;
+
+namespace Hydra.Such.Data.Logic.Nutrition
+{
+    public static class DBRecordTechnicalOfPlates
+    {
+        #region CRUD
+        public static List<FichasTécnicasPratos> GetAll()
+        {
+            try
+            {
+                using (var ctx = new SuchDBContext())
+                {
+                    return ctx.FichasTécnicasPratos.ToList();
+                }
+            }
+            catch (Exception ex)
+            {
+                return null;
+            }
+        }
+        public static List<FichasTécnicasPratos> GetByPlateNo(string PlateNo)
+        {
+            try
+            {
+                using (var ctx = new SuchDBContext())
+                {
+                    return ctx.FichasTécnicasPratos.Where(x => x.NºPrato == PlateNo).ToList();
+                }
+            }
+            catch (Exception ex)
+            {
+                return null;
+            }
+        }
+
+        public static FichasTécnicasPratos Create(FichasTécnicasPratos ObjectToCreate)
+        {
+            try
+            {
+                using (var ctx = new SuchDBContext())
+                {
+                    ObjectToCreate.DataHoraCriação = DateTime.Now;
+                    ctx.FichasTécnicasPratos.Add(ObjectToCreate);
+                    ctx.SaveChanges();
+                }
+
+                return ObjectToCreate;
+            }
+            catch (Exception ex)
+            {
+
+                return null;
+            }
+        }
+
+        public static FichasTécnicasPratos Update(FichasTécnicasPratos ObjectToUpdate)
+        {
+            try
+            {
+                using (var ctx = new SuchDBContext())
+                {
+                    ObjectToUpdate.DataHoraModificação = DateTime.Now;
+                    ctx.FichasTécnicasPratos.Update(ObjectToUpdate);
+                    ctx.SaveChanges();
+                }
+
+                return ObjectToUpdate;
+            }
+            catch (Exception ex)
+            {
+
+                return null;
+            }
+        }
+        public static bool Delete(FichasTécnicasPratos ObjectToDelete)
+        {
+            try
+            {
+                using (var ctx = new SuchDBContext())
+                {
+                    ctx.FichasTécnicasPratos.Remove(ObjectToDelete);
+                    ctx.SaveChanges();
+                }
+
+                return true;
+            }
+            catch (Exception ex)
+            {
+
+                return false;
+            }
+        }
+
+        #endregion
+        public static RecordTechnicalOfPlatesModelView ParseToViewModel(this FichasTécnicasPratos item)
+        {
+            if (item != null)
+            {
+                return new RecordTechnicalOfPlatesModelView()
+                {
+                    PlateNo = item.NºPrato,
+                    Description = item.Descrição,
+                    UnitMeasureCode = item.CódUnidadeMedida,
+                    State = item.Estado,
+                    RecordTechnicalName = item.NomeFichaTécnica,
+                    LocalizationCode = item.CódLocalização,
+                    PreparationTime = item.TempoPreparação,
+                    TechnicalCooking = item.TécnicaCulinária,
+                    Group = item.Grupo,
+                    Epoch = item.Época,
+                    DosesNo = item.NºDeDoses,
+                    PreparationTemperature = item.TemperaturaPreparação,
+                    FinalTemperatureConfection = item.TemperaturaFinalConfeção,
+                    ServeTemperature = item.TemperaturaAServir,
+                    Image = item.Image,
+                    VariationPriceCost = item.VariaçãoPreçoCusto,
+                    ClassFt1 = item.ClassFt1,
+                    ClassFt2 = item.ClassFt2,
+                    ClassFt3 = item.ClassFt3,
+                    ClassFt4 = item.ClassFt4,
+                    ClassFt5 = item.ClassFt5,
+                    ClassFt6 = item.ClassFt6,
+                    ClassFt7 = item.ClassFt7,
+                    ClassFt8 = item.ClassFt8,
+                    CenterResponsibilityCode = item.CódigoCentroResponsabilidade,
+                    Observations = item.Observações,
+                    CreateDateTime = item.DataHoraCriação,
+                    UpdateDateTime = item.DataHoraModificação,
+                    CreateUser = item.UtilizadorCriação,
+                    UpdateUser = item.UtilizadorModificação
+                };
+            }
+            return null;
+        }
+        public static List<RecordTechnicalOfPlatesModelView> ParseToViewModel(this List<FichasTécnicasPratos> items)
+        {
+            List<RecordTechnicalOfPlatesModelView> parsedItems = new List<RecordTechnicalOfPlatesModelView>();
+            if (items != null)
+                items.ForEach(x =>
+                    parsedItems.Add(x.ParseToViewModel()));
+            return parsedItems;
+        }
+    }
+}
---
+++ Hydra.Such.Portal/Hydra.Such.Data/ViewModel/Nutrition/RecordTechnicalOfPlatesModelView.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Hydra.Such.Data.ViewModel.Nutrition
+{
+    public class RecordTechnicalOfPlatesModelView : ErrorHandler
+    {
+        public string PlateNo { get; set; }
+        public string Description { get; set; }
+        public string UnitMeasureCode { get; set; }
+        public int? State { get; set; }
+        public string RecordTechnicalName { get; set; }
+        public string LocalizationCode { get; set; }
+        public int? PreparationTime { get; set; }
+        public int? TechnicalCooking { get; set; }
+        public string Group { get; set; }
+        public string Epoch { get; set; }
+        public int? DosesNo { get; set; }
+        public int? PreparationTemperature { get; set; }
+        public int? FinalTemperatureConfection { get; set; }
+        public int? ServeTemperature { get; set; }
+        public byte[] Image { get; set; }
+        public int? VariationPriceCost { get; set; }
+        public int? ClassFt1 { get; set; }
+        public int? ClassFt2 { get; set; }
+        public string ClassFt3 { get; set; }
+        public string ClassFt4 { get; set; }
+        public string ClassFt5 { get; set; }
+        public string ClassFt6 { get; set; }
+        public string ClassFt7 { get; set; }
+        public string ClassFt8 { get; set; }
+        public string CenterResponsibilityCode { get; set; }
+        public string Observations { get; set; }
+        public DateTime? CreateDateTime { get; set; }
+        public DateTime? UpdateDateTime { get; set; }
+        public string CreateUser { get; set; }
+        public string UpdateUser { get; set; }
+    }
+}
