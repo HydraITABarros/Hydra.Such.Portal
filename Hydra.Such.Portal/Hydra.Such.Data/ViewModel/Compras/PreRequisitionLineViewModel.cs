645b7dfbab9ea1d218df58dbfe54321157fe5341
Daniel Alves <dribeiro@hydra.pt>
2018-02-02 11:18:11 +0000

    DR

--- Hydra.Such.Portal/Hydra.Such.Data/ViewModel/Compras/PreRequisitionLineViewModel.cs
+++ Hydra.Such.Portal/Hydra.Such.Data/ViewModel/Compras/PreRequisitionLineViewModel.cs
@@ -40,7 +40,7 @@ namespace Hydra.Such.Data.ViewModel.Compras
         public string CustomerNo { get; set; }
         public string OpenOrderNo { get; set; }
         public int? OpenOrderLineNo { get; set; }
-
+        public decimal? TotalCost { get; set; }
         //EXTRAS
         public bool Selected { get; set; }
     }
