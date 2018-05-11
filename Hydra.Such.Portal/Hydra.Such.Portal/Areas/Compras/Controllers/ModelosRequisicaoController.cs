65ff3971312933433daf50c67c327cba42a57b7e
João Pereira <jpereira@hydra.pt>
2018-05-11 11:14:46 +0100

    Correção PreRequisições; Modelos Requisição

--- Hydra.Such.Portal/Hydra.Such.Portal/Areas/Compras/Controllers/ModelosRequisicaoController.cs
+++ Hydra.Such.Portal/Hydra.Such.Portal/Areas/Compras/Controllers/ModelosRequisicaoController.cs
@@ -15,71 +15,71 @@ namespace Hydra.Such.Portal.Areas.Compras.Controllers
 {
     public class ModelosRequisicaoController : Controller
     {
-        //[Area("Compras")]
-        //public IActionResult Index()
-        //{
-        //    UserAccessesViewModel userPermissions =
-        //        DBUserAccesses.GetByUserAreaFunctionality(User.Identity.Name, Enumerations.Areas.Compras, Enumerations.Features.ModelosRequisicao);
-        //    if (userPermissions != null && userPermissions.Read.Value)
-        //    {
-        //        ViewBag.UPermissions = userPermissions;
-        //        return View();
-        //    }
-        //    else
-        //    {
-        //        return Redirect(Url.Content("~/Error/AccessDenied"));
-        //    }
-        //}
-        
-        //[Area("Compras")]
-        //public IActionResult Detalhes()
-        //{
-        //    UserAccessesViewModel userPermissions =
-        //        DBUserAccesses.GetByUserAreaFunctionality(User.Identity.Name, Enumerations.Areas.Compras, Enumerations.Features.ModelosRequisicao);
-        //    if (userPermissions != null && userPermissions.Read.Value)
-        //    {
-        //        ViewBag.UPermissions = userPermissions;
-        //        return View();
-        //    }
-        //    else
-        //    {
-        //        return Redirect(Url.Content("~/Error/AccessDenied"));
-        //    }
-        //}
+        [Area("Compras")]
+        public IActionResult Index()
+        {
+            UserAccessesViewModel userPermissions =
+                DBUserAccesses.GetByUserAreaFunctionality(User.Identity.Name, Enumerations.Areas.Compras, Enumerations.Features.ModelosRequisicao);
+            if (userPermissions != null && userPermissions.Read.Value)
+            {
+                ViewBag.UPermissions = userPermissions;
+                return View();
+            }
+            else
+            {
+                return Redirect(Url.Content("~/Error/AccessDenied"));
+            }
+        }
 
-        //[HttpPost]
-        //[Area("Compras")]
-        //public JsonResult GetRequisitionTemplates()
-        //{
-        //    List<RequisitionViewModel> result = DBRequestModels.GetAll().ParseToViewModel();
+        [Area("Compras")]
+        public IActionResult Detalhes()
+        {
+            UserAccessesViewModel userPermissions =
+                DBUserAccesses.GetByUserAreaFunctionality(User.Identity.Name, Enumerations.Areas.Compras, Enumerations.Features.ModelosRequisicao);
+            if (userPermissions != null && userPermissions.Read.Value)
+            {
+                ViewBag.UPermissions = userPermissions;
+                return View();
+            }
+            else
+            {
+                return Redirect(Url.Content("~/Error/AccessDenied"));
+            }
+        }
 
-        //    //Apply User Dimensions Validations
-        //    List<AcessosDimensões> userDimensions = DBUserDimensions.GetByUserId(User.Identity.Name);
-        //    //Regions
-        //    if (userDimensions.Where(y => y.Dimensão == (int)Dimensions.Region).Count() > 0)
-        //        result.RemoveAll(x => !userDimensions.Any(y => y.Dimensão == (int)Dimensions.Region && y.ValorDimensão == x.RegionCode));
-        //    //FunctionalAreas
-        //    if (userDimensions.Where(y => y.Dimensão == (int)Dimensions.FunctionalArea).Count() > 0)
-        //        result.RemoveAll(x => !userDimensions.Any(y => y.Dimensão == (int)Dimensions.FunctionalArea && y.ValorDimensão == x.FunctionalAreaCode));
-        //    //ResponsabilityCenter
-        //    if (userDimensions.Where(y => y.Dimensão == (int)Dimensions.ResponsabilityCenter).Count() > 0)
-        //        result.RemoveAll(x => !userDimensions.Any(y => y.Dimensão == (int)Dimensions.ResponsabilityCenter && y.ValorDimensão == x.CenterResponsibilityCode));
-        //    return Json(result);
-        //}
+        [HttpPost]
+        [Area("Compras")]
+        public JsonResult GetRequisitionTemplates()
+        {
+            List<RequisitionViewModel> result = DBRequestModels.GetAll().ParseToViewModel();
 
-        //[HttpPost]
-        //[Area("Compras")]
-        //public JsonResult GetRequisition([FromBody] string id)
-        //{
-        //    RequisitionViewModel item;
-        //    if (!string.IsNullOrEmpty(id) && id != "0")
-        //    {
-        //        item = DBRequestModels.GetById(id).ParseToViewModel();
-        //    }
-        //    else
-        //        item = new RequisitionViewModel();
+            //Apply User Dimensions Validations
+            List<AcessosDimensões> userDimensions = DBUserDimensions.GetByUserId(User.Identity.Name);
+            //Regions
+            if (userDimensions.Where(y => y.Dimensão == (int)Dimensions.Region).Count() > 0)
+                result.RemoveAll(x => !userDimensions.Any(y => y.Dimensão == (int)Dimensions.Region && y.ValorDimensão == x.RegionCode));
+            //FunctionalAreas
+            if (userDimensions.Where(y => y.Dimensão == (int)Dimensions.FunctionalArea).Count() > 0)
+                result.RemoveAll(x => !userDimensions.Any(y => y.Dimensão == (int)Dimensions.FunctionalArea && y.ValorDimensão == x.FunctionalAreaCode));
+            //ResponsabilityCenter
+            if (userDimensions.Where(y => y.Dimensão == (int)Dimensions.ResponsabilityCenter).Count() > 0)
+                result.RemoveAll(x => !userDimensions.Any(y => y.Dimensão == (int)Dimensions.ResponsabilityCenter && y.ValorDimensão == x.CenterResponsibilityCode));
+            return Json(result);
+        }
 
-        //    return Json(item);
-        //}
+        [HttpPost]
+        [Area("Compras")]
+        public JsonResult GetRequisition([FromBody] string id)
+        {
+            RequisitionViewModel item;
+            if (!string.IsNullOrEmpty(id) && id != "0")
+            {
+                item = DBRequestModels.GetById(id).ParseToViewModel();
+            }
+            else
+                item = new RequisitionViewModel();
+
+            return Json(item);
+        }
     }
 }
