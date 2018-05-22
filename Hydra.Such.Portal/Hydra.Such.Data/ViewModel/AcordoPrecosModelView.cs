a06e6e6016067220ba318b862a3de581fd1ea45d
Amaro Romão <ARomao@such.pt>
2018-05-22 13:26:43 +0100

    Acordo Preços - 22/05/2018

---
+++ Hydra.Such.Portal/Hydra.Such.Data/ViewModel/AcordoPrecosModelView.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Hydra.Such.Data.ViewModel
+{
+    public class AcordoPrecosModelView
+    {
+        public string NoProcedimento { get; set; }
+        public DateTime? DtInicio { get; set; }
+        public string DtInicioTexto { get; set; }
+        public DateTime? DtFim { get; set; }
+        public string DtFimTexto { get; set; }
+        public decimal? ValorTotal { get; set; }
+    }
+}
--- Hydra.Such.Portal/Hydra.Such.Portal/Controllers/AdministracaoController.cs
+++ Hydra.Such.Portal/Hydra.Such.Portal/Controllers/AdministracaoController.cs
@@ -2837,9 +2837,10 @@ namespace Hydra.Such.Portal.Controllers
         #endregion
 
         #region Acordo de Preços
-        public IActionResult AcordoPrecos(string id)
+
+        public IActionResult AcordoPrecos_List()
         {
-            UserAccessesViewModel UPerm = GetPermissions(id);
+            UserAccessesViewModel UPerm = GetPermissions("Administracao");
             if (UPerm != null && UPerm.Read.Value)
             {
                 ViewBag.CreatePermissions = !UPerm.Create.Value;
@@ -2851,6 +2852,20 @@ namespace Hydra.Such.Portal.Controllers
             {
                 return RedirectToAction("AccessDenied", "Error");
             }
+        }
+
+        public IActionResult AcordoPrecos(string id)
+        {
+            ViewBag.NoProcedimento = id;
+            return View();
+        }
+
+        [HttpPost]
+        public JsonResult GetListAcordoPrecos()
+        {
+            List<AcordoPrecosModelView> result = DBAcordoPrecos.GetAll();
+
+            return Json(result);
         }
 
         [HttpPost]
@@ -2868,6 +2883,32 @@ namespace Hydra.Such.Portal.Controllers
                 return Json(0);
             else
                 return Json(1);
+        }
+
+        [HttpPost]
+        public JsonResult DeleteAcordoPreco([FromBody] AcordoPrecosModelView data)
+        {
+            int result = 0;
+            bool dbDeleteLinhaResult = false;
+            bool dbDeleteFornecedorResult = false;
+            bool dbDeleteAcordoPrecoResult = false;
+
+            try
+            {
+                dbDeleteLinhaResult = DBLinhasAcordoPrecos.DeleteByProcedimento(data.NoProcedimento);
+
+                dbDeleteFornecedorResult = DBFornecedoresAcordoPrecos.DeleteByProcedimento(data.NoProcedimento);
+
+                dbDeleteAcordoPrecoResult = DBAcordoPrecos.Delete(data.NoProcedimento);
+
+                if (!dbDeleteAcordoPrecoResult)
+                    result = 1;
+            }
+            catch (Exception ex)
+            {
+                result = 99;
+            }
+            return Json(result);
         }
 
         [HttpPost]
