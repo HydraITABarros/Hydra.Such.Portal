0229fc7b5fe769aa93e3d40bc39a26d4bf72d275
João Fernandes <joao.fernandes@geratriz.pt>
2018-06-01 12:41:48 +0100

    Create: NAV webservice ShipToAddress reference and service

---
+++ Hydra.Such.Portal/Hydra.Such.Data/NAV/WSShipToAddressService.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ServiceModel;
+using System.Text;
+
+namespace Hydra.Such.Data.NAV
+{
+    public static class WSShipToAddressService
+    {
+        static BasicHttpBinding navWSBinding;
+
+        static WSShipToAddressService()
+        {
+            ///Configure Basic Binding to have access to NAV
+            navWSBinding = new BasicHttpBinding();
+            navWSBinding.Security.Mode = BasicHttpSecurityMode.TransportCredentialOnly;
+            navWSBinding.Security.Transport.ClientCredentialType = HttpClientCredentialType.Windows;
+            navWSBinding.MaxReceivedMessageSize = 20971520;
+            /*navWSBinding.MaxBufferSize = 20971520;
+            navWSBinding.MaxBufferPoolSize = 20971520;*/
+        }
+    }
+}
