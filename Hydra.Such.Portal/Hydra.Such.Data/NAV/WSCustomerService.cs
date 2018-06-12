afec0faee1357abce20faf08f2cad60e0d2d6801
André Bastos <andre.bastos.geratriz@outlook.pt>
2018-06-12 11:17:47 +0100

    Update: adds customers to menu Processos DRs

--- Hydra.Such.Portal/Hydra.Such.Data/NAV/WSCustomerService.cs
+++ Hydra.Such.Portal/Hydra.Such.Data/NAV/WSCustomerService.cs
@@ -20,7 +20,9 @@ namespace Hydra.Such.Data.NAV
             navWSBinding = new BasicHttpBinding();
             navWSBinding.Security.Mode = BasicHttpSecurityMode.TransportCredentialOnly;
             navWSBinding.Security.Transport.ClientCredentialType = HttpClientCredentialType.Windows;
-            navWSBinding.MaxReceivedMessageSize = 20971520;
+            //navWSBinding.MaxReceivedMessageSize = 20971520;
+            navWSBinding.MaxReceivedMessageSize = int.MaxValue;
+
             /*navWSBinding.MaxBufferSize = 20971520;
             navWSBinding.MaxBufferPoolSize = 20971520;*/
         }
