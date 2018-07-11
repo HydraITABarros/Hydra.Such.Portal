486e7f704048a2be2cb576908a5318e7817240ae
DanielAlvesRibeiro <dribeiro@hydra.pt>
2018-07-11 15:17:45 +0100

    DR

---
+++ Hydra.Such.Portal/Hydra.Such.Data/ViewModel/NAVContractInvoiceHeaderViewModel.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Hydra.Such.Data.ViewModel
+{
+    public class NAVContractInvoiceHeaderViewModel
+    {
+        public string No_ { get; set; }
+        public decimal? ValorContrato { get; set; }
+    }
+}
