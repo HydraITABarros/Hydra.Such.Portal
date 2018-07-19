1b28016c7adb42b42a4862c2b7c7363a03f0c0f6
DanielAlvesRibeiro <dribeiro@hydra.pt>
2018-07-19 11:18:36 +0100

    DR

--- Hydra.Such.Portal/Hydra.Such.Portal/Controllers/FaturacaoController.cs
+++ Hydra.Such.Portal/Hydra.Such.Portal/Controllers/FaturacaoController.cs
@@ -50,6 +50,9 @@ namespace Hydra.Such.Portal.Controllers
         public IActionResult DetalhesRecFatura(string id)
         {
             UserAccessesViewModel UPerm = DBUserAccesses.GetByUserAreaFunctionality(User.Identity.Name, Enumerations.Features.ReceçãoFaturação);
+            //UserConfigurationsViewModel userConfig = DBUserConfigurations.GetById(User.Identity.Name);
+
+
             if (UPerm != null && UPerm.Read.Value)
             {
                 ViewBag.Id = id;
@@ -87,6 +90,19 @@ namespace Hydra.Such.Portal.Controllers
 
             return Json(result);
         }
+
+        [HttpGet]
+        public JsonResult GetProblemsByUserProfile()
+        {
+            List<DDMessageString> result = billingRecService.GetProblem().Select(x => new DDMessageString()
+            {
+                id = x.Tipo,
+                value = x.Descricao
+            }).ToList();
+
+            return Json(result);
+        }
+
         [HttpGet]
         public JsonResult GetReasons()
         {
