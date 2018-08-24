70245296ac8a2bd47a51a224b202ba0215c1f5c8
rfpeixoto <rfpeixoto@RFPEIXOTO-PC>
2018-08-24 17:48:01 +0100

    RP

--- Hydra.Such.Portal/Hydra.Such.Portal/Controllers/ContratosController.cs
+++ Hydra.Such.Portal/Hydra.Such.Portal/Controllers/ContratosController.cs
@@ -1564,7 +1564,7 @@ namespace Hydra.Such.Portal.Controllers
                             {
                                 List<RequisiçõesClienteContrato> ListaContratos = DBContractClientRequisition.GetByContract(item.NºDeContrato);
                                 RequisiçõesClienteContrato Reqcontract = ListaContratos.Find(x => x.GrupoFatura == line.GrupoFatura && x.DataInícioCompromisso <= item.ÚltimaDataFatura && x.DataFimCompromisso >= item.ÚltimaDataFatura);
-                                if(Reqcontract == null &&(Reqcontract.NºRequisiçãoCliente==null || Reqcontract.NºRequisiçãoCliente == "")) {
+                                if(Reqcontract == null ) {
                                     Problema += "Falta Nota Encomenda";
                                 }
                             }
