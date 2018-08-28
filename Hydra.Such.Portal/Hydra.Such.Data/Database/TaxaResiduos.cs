43d5a37e98c60919d1daa9c9a0d265dd31b61506
João Pereira <jpereira@hydra.pt>
2018-08-28 18:33:05 +0100

    Autorização Faturação

---
+++ Hydra.Such.Portal/Hydra.Such.Data/Database/TaxaResiduos.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Hydra.Such.Data.Database
+{
+    public partial class TaxaResiduos
+    {
+        public string Recurso { get; set; }
+        public string FamiliaRecurso { get; set; }
+        public DateTime? Data { get; set; }
+        public DateTime? DataHoraCriação { get; set; }
+        public string UtilizadorCriação { get; set; }
+        public DateTime? DataHoraModificação { get; set; }
+        public string UtilizadorModificação { get; set; }
+    }
+}
