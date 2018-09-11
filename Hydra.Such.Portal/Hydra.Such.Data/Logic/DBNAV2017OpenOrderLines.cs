534a0444cd62e9047bc0f0f97d2dd7eda3545883
João Pereira <jpereira@hydra.pt>
2018-09-11 09:36:07 +0100

    .

--- Hydra.Such.Portal/Hydra.Such.Data/Logic/DBNAV2017OpenOrderLines.cs
+++ Hydra.Such.Portal/Hydra.Such.Data/Logic/DBNAV2017OpenOrderLines.cs
@@ -9,7 +9,7 @@ namespace Hydra.Such.Data.Logic
 {
    public class DBNAV2017OpenOrderLines
     {
-        public static List<NAVOpenOrderLinesViewModels> GetAll(string NAVDatabaseName, string NAVCompanyName, string Date, string PurchaseHeaderNo,string codFuncitonalArea)
+        public static List<NAVOpenOrderLinesViewModels> GetAll(string NAVDatabaseName, string NAVCompanyName, string Date, string PurchaseHeaderNo,string codFuncitonalArea, bool onlyWithAvailableQuantity = false)
         {
             List<NAVOpenOrderLinesViewModels> result = new List<NAVOpenOrderLinesViewModels>();
             try
@@ -25,9 +25,10 @@ namespace Hydra.Such.Data.Logic
                         new SqlParameter("@DateSupplierPrice", Date),
                         new SqlParameter("@FunctionalAreaCode", codFunc),
                         new SqlParameter("@PurchaseHeaderNo", purhNo),
-                    };
+                        new SqlParameter("@OnlyWithAvailableQuantity", (onlyWithAvailableQuantity ? 1 : 0)),
+            };
 
-                    IEnumerable<dynamic> data = ctx.execStoredProcedure("exec NAV2017LinhasEncomendaAberto @DBName, @CompanyName, @DateSupplierPrice, @FunctionalAreaCode,@PurchaseHeaderNo", parameters);
+                    IEnumerable<dynamic> data = ctx.execStoredProcedure("exec NAV2017LinhasEncomendaAberto @DBName, @CompanyName, @DateSupplierPrice, @FunctionalAreaCode,@PurchaseHeaderNo,@OnlyWithAvailableQuantity", parameters);
 
                     foreach (dynamic temp in data)
                     {
@@ -89,66 +90,72 @@ namespace Hydra.Such.Data.Logic
                         //item.ProdOrderLineN = (int)temp.ProdOrderLineN;
                         //item.ContractStartingDate = (DateTime)temp.ContractStartingDate;
                         //item.ContractEndingDate = (DateTime)temp.ContractEndingDate;
-                        
+
                         result.Add(new NAVOpenOrderLinesViewModels()
-                            {
-                                id = temp.DocumentNO + " " + temp.Line_No + " " + temp.Numb,
-                                DocumentType = (int)temp.DocumentType,
-                                DocumentNO = (string)temp.DocumentNO,
-                                Line_No = (int)temp.Line_No,
-                                BuyFromVendorNo = (string)temp.BuyFromVendorNo,
-                                Type = (int)temp.Type,
-                                Numb = (string)temp.Numb,
-                                LocationCode = (string)temp.LocationCode,
-                                ExpectedReceiptDate = (DateTime)temp.ExpectedReceiptDate,
-                                Description = (string)temp.Description,
-                                UnitofMeasure = (string)temp.UnitofMeasure,
-                                Quantity = (decimal)temp.Quantity,
-                                OutstandingQuantity = (decimal)temp.OutstandingQuantity,
-                                QtytoInvoice = (decimal)temp.QtytoInvoice,
-                                QtytoReceive = (decimal)temp.QtytoReceive,
-                                DirectUnitCost = (decimal)temp.DirectUnitCost,
-                                UnitCostLCY = (decimal)temp.UnitCostLCY,
-                                Amount = (decimal)temp.Amount,
-                                UnitPriceLCY = (decimal)temp.UnitPriceLCY,
-                                OutstandingAmount = (decimal)temp.OutstandingAmount,
-                                QuantityReceived = (decimal)temp.QuantityReceived,
-                                QuantityInvoiced = (decimal)temp.QuantityInvoiced,
-                                PaytoVendorNo = (string)temp.PaytoVendorNo,
-                                VendorItemNo = (string)temp.VendorItemNo,
-                                GenBusPostingGroup = (string)temp.GenBusPostingGroup,
-                                GenProdPostingGroup = (string)temp.GenProdPostingGroup,
-                                EntryPoint = (string)temp.EntryPoint,
-                                CurrencyCode = (string)temp.CurrencyCode,
-                                VATBaseAmount = (decimal)temp.VATBaseAmount,
-                                UnitCost = (decimal)temp.UnitCost,
-                                SystemCreatedEntry = (int)temp.SystemCreatedEntry,
-                                LineAmount = (decimal)temp.LineAmount,
-                                ICPartnerRefType = (int)temp.ICPartnerRefType,
-                                ICPartnerReference = (string)temp.ICPartnerReference,
-                                ICPartnerCode = (string)temp.ICPartnerCode,
-                                DimensionSetID = (int)temp.DimensionSetID,
-                                ReturnsDeferralStartDate = (DateTime)temp.ReturnsDeferralStartDate,
-                                ProdOrderNo = (string)temp.ProdOrderNo,
-                                QtyperUnitofMeasure = (decimal)temp.QtyperUnitofMeasure,
-                                UnitofMeasureCode = (string)temp.UnitofMeasureCode,
-                                QuantityBase = (decimal)temp.QuantityBase,
-                                OutstandingQtyBase = (decimal)temp.OutstandingQtyBase,
-                                QtytoInvoiceBase = (decimal)temp.QtytoInvoiceBase,
-                                QtytoReceiveBase = (decimal)temp.QtytoReceiveBase,
-                                QtyRcdNotInvoicedBase = (decimal)temp.QtyRcdNotInvoicedBase,
-                                QtyInvoicedBase = (decimal)temp.QtyInvoicedBase,
-                                SalvageValue = (decimal)temp.SalvageValue,
-                                UnitofMeasureCrossRef = (string)temp.UnitofMeasureCrossRef,
-                                ItemCategoryCode = (string)temp.ItemCategoryCode,
-                                ProductGroupCode = (string)temp.ProductGroupCode,
-                                OrderDate = (DateTime)temp.OrderDate,
-                                WorkCenterNo = (string)temp.WorkCenterNo,
-                                Finished = (int)temp.Finished,
-                                ProdOrderLineN = (int)temp.ProdOrderLineN,
-                                ContractStartingDate = (DateTime)temp.ContractStartingDate,
-                                ContractEndingDate = (DateTime)temp.ContractEndingDate
-                            });
+                        {
+                            id = temp.DocumentNO + " " + temp.Line_No + " " + temp.Numb,
+                            DocumentType = (int)temp.DocumentType,
+                            DocumentNO = (string)temp.DocumentNO,
+                            Line_No = (int)temp.Line_No,
+                            BuyFromVendorNo = (string)temp.BuyFromVendorNo,
+                            Type = (int)temp.Type,
+                            Numb = (string)temp.Numb,
+                            LocationCode = (string)temp.LocationCode,
+                            ExpectedReceiptDate = (DateTime)temp.ExpectedReceiptDate,
+                            Description = (string)temp.Description,
+                            UnitofMeasure = (string)temp.UnitofMeasure,
+                            Quantity = (decimal)temp.Quantity,
+                            OutstandingQuantity = (decimal)temp.OutstandingQuantity,
+                            QtytoInvoice = (decimal)temp.QtytoInvoice,
+                            QtytoReceive = (decimal)temp.QtytoReceive,
+                            DirectUnitCost = (decimal)temp.DirectUnitCost,
+                            UnitCostLCY = (decimal)temp.UnitCostLCY,
+                            Amount = (decimal)temp.Amount,
+                            UnitPriceLCY = (decimal)temp.UnitPriceLCY,
+                            OutstandingAmount = (decimal)temp.OutstandingAmount,
+                            QuantityReceived = (decimal)temp.QuantityReceived,
+                            QuantityInvoiced = (decimal)temp.QuantityInvoiced,
+                            PaytoVendorNo = (string)temp.PaytoVendorNo,
+                            VendorItemNo = (string)temp.VendorItemNo,
+                            GenBusPostingGroup = (string)temp.GenBusPostingGroup,
+                            GenProdPostingGroup = (string)temp.GenProdPostingGroup,
+                            EntryPoint = (string)temp.EntryPoint,
+                            CurrencyCode = (string)temp.CurrencyCode,
+                            VATBaseAmount = (decimal)temp.VATBaseAmount,
+                            UnitCost = (decimal)temp.UnitCost,
+                            SystemCreatedEntry = (int)temp.SystemCreatedEntry,
+                            LineAmount = (decimal)temp.LineAmount,
+                            ICPartnerRefType = (int)temp.ICPartnerRefType,
+                            ICPartnerReference = (string)temp.ICPartnerReference,
+                            ICPartnerCode = (string)temp.ICPartnerCode,
+                            DimensionSetID = (int)temp.DimensionSetID,
+                            ReturnsDeferralStartDate = (DateTime)temp.ReturnsDeferralStartDate,
+                            ProdOrderNo = (string)temp.ProdOrderNo,
+                            QtyperUnitofMeasure = (decimal)temp.QtyperUnitofMeasure,
+                            UnitofMeasureCode = (string)temp.UnitofMeasureCode,
+                            QuantityBase = (decimal)temp.QuantityBase,
+                            OutstandingQtyBase = (decimal)temp.OutstandingQtyBase,
+                            QtytoInvoiceBase = (decimal)temp.QtytoInvoiceBase,
+                            QtytoReceiveBase = (decimal)temp.QtytoReceiveBase,
+                            QtyRcdNotInvoicedBase = (decimal)temp.QtyRcdNotInvoicedBase,
+                            QtyInvoicedBase = (decimal)temp.QtyInvoicedBase,
+                            SalvageValue = (decimal)temp.SalvageValue,
+                            UnitofMeasureCrossRef = (string)temp.UnitofMeasureCrossRef,
+                            ItemCategoryCode = (string)temp.ItemCategoryCode,
+                            ProductGroupCode = (string)temp.ProductGroupCode,
+                            OrderDate = (DateTime)temp.OrderDate,
+                            WorkCenterNo = (string)temp.WorkCenterNo,
+                            Finished = (int)temp.Finished,
+                            ProdOrderLineN = (int)temp.ProdOrderLineN,
+                            ContractStartingDate = (DateTime)temp.ContractStartingDate,
+                            ContractEndingDate = (DateTime)temp.ContractEndingDate,
+                            FunctionalAreaNo = temp.FunctionalAreaNo.Equals(DBNull.Value) ? "" : (string)temp.FunctionalAreaNo,
+                            FunctionalAreaDescription = temp.FunctionalAreaDescription.Equals(DBNull.Value) ? "" : (string)temp.FunctionalAreaDescription,
+                            RegionNo = temp.RegionNo.Equals(DBNull.Value) ? "" : (string)temp.RegionNo,
+                            RegionDescription = temp.RegionDescription.Equals(DBNull.Value) ? "" : (string)temp.RegionDescription,
+                            ResponsabilityCenterNo = temp.ResponsabilityCenterNo.Equals(DBNull.Value) ? "" : (string)temp.ResponsabilityCenterNo,
+                            ResponsabilityCenterDescription = temp.ResponsabilityCenterDescription.Equals(DBNull.Value) ? "" : (string)temp.ResponsabilityCenterDescription
+                        });
                     }
                     return result;
                 }
