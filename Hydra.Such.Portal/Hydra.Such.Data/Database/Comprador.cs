58040c7e73246ee1675e43c247eaefbcbba722e8
nrato <samthesung@gmail.com>
2018-10-03 10:37:33 +0100

    NR20181003

---
+++ Hydra.Such.Portal/Hydra.Such.Data/Database/Comprador.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Hydra.Such.Data.Database
+{
+    public partial class Comprador
+    {
+        public string CodComprador { get; set; }
+        public string NomeComprador { get; set; }
+    }
+}
