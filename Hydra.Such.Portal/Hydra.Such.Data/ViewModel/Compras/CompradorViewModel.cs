064ab6146851d0973fdb41a9726f38682b751532
Amaro Romão <ARomao@such.pt>
2018-10-03 15:18:48 +0100

    ARomao@such.pt

---
+++ Hydra.Such.Portal/Hydra.Such.Data/ViewModel/Compras/CompradorViewModel.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Hydra.Such.Data.ViewModel.Compras
+{
+    public class CompradorViewModel : ErrorHandler
+    {
+        public string CodComprador { get; set; }
+        public string NomeComprador { get; set; }
+        public DateTime? DataHoraCriacao { get; set; }
+        public DateTime? DataHoraModificacao { get; set; }
+        public string UtilizadorCriacao { get; set; }
+        public string UtilizadorModificacao { get; set; }
+    }
+}
