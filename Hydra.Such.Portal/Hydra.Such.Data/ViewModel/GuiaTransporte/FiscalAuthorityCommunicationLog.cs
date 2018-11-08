fb7304364db57313371d69eb08e602a15cd670ab
José Paulo Monteiro <JMonteiro@such.pt>
2018-11-08 13:59:44 +0000

    Impressão Guias Transporte NAV - Layout final

--- Hydra.Such.Portal/Hydra.Such.Data/ViewModel/GuiaTransporte/FiscalAuthorityCommunicationLog.cs
+++ Hydra.Such.Portal/Hydra.Such.Data/ViewModel/GuiaTransporte/FiscalAuthorityCommunicationLog.cs
@@ -11,6 +11,7 @@ namespace Hydra.Such.Data.ViewModel.GuiaTransporte
     {
         public string SourceNo { get; set; }
         public string DocumentCodeId { get; set; }
+        [JsonConverter(typeof(DateFormatConverter), "dd-MM-yyyy HH:mm:ss")]
         public DateTime CommunicationDateTime { get; set; }
         public string ReturnCode { get; set; }
         public string ReturnMessage { get; set; }
