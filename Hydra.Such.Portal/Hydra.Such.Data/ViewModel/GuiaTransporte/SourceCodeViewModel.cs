101e44c80957f2831f8e4e7ca7b9e562f05b5594
José Paulo Monteiro <JMonteiro@such.pt>
2019-01-10 19:03:52 +0000

    Guias de Transporte NAV

---
+++ Hydra.Such.Portal/Hydra.Such.Data/ViewModel/GuiaTransporte/SourceCodeViewModel.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Hydra.Such.Data.ViewModel.GuiaTransporte
+{
+    public class SourceCodeViewModel
+    {
+        public string SourceCode { get; set; }
+        public string SourceDescription { get; set; }
+    }
+}
