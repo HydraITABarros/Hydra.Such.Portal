a87a569daad58baa4fe263c108410886a797a6aa
José Paulo Monteiro <JMonteiro@such.pt>
2019-02-12 12:32:13 +0000

    Guias de transporte

---
+++ Hydra.Such.Portal/Hydra.Such.Data/ViewModel/NAVPostCode.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Hydra.Such.Data.ViewModel
+{
+    public class NAVPostCode
+    {
+        public string PostCode { get; set; }
+        public string City { get; set; }
+    }
+}
