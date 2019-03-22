ff7e388582eeff4345a9db798a8cf5e31e65a440
André Bastos <andre.bastos.geratriz@outlook.pt>
2019-03-22 11:53:11 +0000

    Fix NavigationFilter avoid controller error on Rest routes

--- Hydra.Such.Portal/Hydra.Such.Portal/Filters/NavigationFilter.cs
+++ Hydra.Such.Portal/Hydra.Such.Portal/Filters/NavigationFilter.cs
@@ -21,24 +21,27 @@ namespace Hydra.Such.Portal.Filters
 
             var httpMethod = context.HttpContext.Request.Method;
             var user = context.HttpContext.User;
-
-            if (httpMethod == "GET" && user.Identity.IsAuthenticated)
+            dynamic viewBag = null;
+            try { viewBag = ((Controller)(context.Controller)).ViewBag; } catch { }
+            if (httpMethod == "GET" && user.Identity.IsAuthenticated && viewBag != null )
             {
+                var teste = context.Controller.GetType();
                 var session = context.HttpContext.Session;
-                var viewBag = ((Controller)(context.Controller)).ViewBag;
                 var menu = new List<MenuViewModel>();
 
-                if (session.GetString("menu") == null || true )
+                if (session.GetString("menu") == null || true)
                 {
                     menu = DBMenu.GetAllByUserId(user.Identity.Name).ParseToViewModel();
                     session.SetString("menu", JsonConvert.SerializeObject(menu));
-                } else
+                }
+                else
                 {
                     menu = JsonConvert.DeserializeObject<List<MenuViewModel>>(session.GetString("menu"));
                 }
                 viewBag._menu = menu;
             }
-            base.OnActionExecuting(context);}
+            base.OnActionExecuting(context);
+        }
 
     }
 }
