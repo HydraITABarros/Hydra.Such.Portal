018b87eb7bcef2055496fbbe8f745df5a7bc08f5
ARomao@such.pt <ARomao@such.pt>
2019-03-25 14:23:42 +0000

    ARomao@such.pt

--- Hydra.Such.Portal/Hydra.Such.Portal/Controllers/ContratosController.cs
+++ Hydra.Such.Portal/Hydra.Such.Portal/Controllers/ContratosController.cs
@@ -251,7 +251,6 @@ namespace Hydra.Such.Portal.Controllers
         public IActionResult AvencaFixa()
         {
             UserAccessesViewModel UPerm = DBUserAccesses.GetByUserAreaFunctionality(User.Identity.Name, Enumerations.Features.ContratosAvencaFixa);
-
             if (UPerm != null && UPerm.Read.Value)
             {
                 ViewBag.UPermissions = UPerm;
