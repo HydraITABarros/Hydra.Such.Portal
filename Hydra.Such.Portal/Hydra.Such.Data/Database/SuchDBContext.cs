7f46e69ad1826367423b7f1e6b9ef54428b627e7
ARomao@such.pt <ARomao@such.pt>
2019-04-01 10:22:27 +0100

    ARomao_such.pt

--- Hydra.Such.Portal/Hydra.Such.Data/Database/SuchDBContext.cs
+++ Hydra.Such.Portal/Hydra.Such.Data/Database/SuchDBContext.cs
@@ -7614,7 +7614,8 @@ namespace Hydra.Such.Data.Database
             });
 
             #region zpgm.28032019
-            modelBuilder.Entity<TipoProcedimentoCcp>(entity => {
+            modelBuilder.Entity<TipoProcedimentoCcp>(entity =>
+            {
                 entity.HasKey(e => e.IdTipo);
 
                 entity.ToTable("TipoProcedimentoCcp");
@@ -7627,7 +7628,8 @@ namespace Hydra.Such.Data.Database
 
             });
 
-            modelBuilder.Entity<FundamentoLegalTipoProcedimentoCcp>(entity => {
+            modelBuilder.Entity<FundamentoLegalTipoProcedimentoCcp>(entity =>
+            {
                 entity.HasKey(e => new { e.IdTipo, e.IdFundamento });
 
                 entity.ToTable("FundamentoLegalTipoProcedimentoCcp");
@@ -7643,7 +7645,8 @@ namespace Hydra.Such.Data.Database
                     .HasConstraintName("FK_FundamentoLegalTipoProcedimentoCcp_TipoProcedimentoCcp");
             });
 
-            modelBuilder.Entity<LoteProcedimentoCcp>(entity => {
+            modelBuilder.Entity<LoteProcedimentoCcp>(entity =>
+            {
                 entity.HasKey(e => new { e.NoProcedimento, e.IdLote });
 
                 entity.ToTable("LoteProcedimentoCcp");
