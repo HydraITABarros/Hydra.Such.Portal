35dd140a09561dbbec72b8bfd5a15471dc6683f0
andre.bastos.geratriz@outlook.pt <andre.bastos.geratriz@outlook.pt>
2019-04-09 07:25:08 -0700

    adds javascriptSwitcher and react.asp.net to project

--- Hydra.Such.Portal/Hydra.Such.Portal/Startup.cs
+++ Hydra.Such.Portal/Hydra.Such.Portal/Startup.cs
@@ -24,6 +24,9 @@ using Microsoft.EntityFrameworkCore;
 using Microsoft.AspNet.OData.Builder;
 using Microsoft.OData.Edm;
 using Microsoft.AspNet.OData.Extensions;
+using JavaScriptEngineSwitcher.ChakraCore;
+using JavaScriptEngineSwitcher.Extensions.MsDependencyInjection;
+using React.AspNet;
 
 namespace Hydra.Such.Portal
 {
@@ -45,6 +48,12 @@ namespace Hydra.Such.Portal
         {
 
             services.AddOData();
+
+            services.AddSingleton<IHttpContextAccessor, HttpContextAccessor>();
+            services.AddReact();
+            // Make sure a JS engine is registered, or you will get an error!
+            services.AddJsEngineSwitcher(options => options.DefaultEngineName = ChakraCoreJsEngine.EngineName)
+              .AddChakraCore();
 
             services.AddMvc(options => {
                 options.Filters.Add(new NavigationFilter());
@@ -111,6 +120,26 @@ namespace Hydra.Such.Portal
             app.UseSession();
 
             app.UseStaticFiles();
+
+            // Initialise ReactJS.NET. Must be before static files.
+            app.UseReact(config =>
+            {
+                // If you want to use server-side rendering of React components,
+                // add all the necessary JavaScript files here. This includes
+                // your components as well as all of their dependencies.
+                // See http://reactjs.net/ for more information. Example:
+                //config
+                //  .AddScript("~/Scripts/First.jsx")
+                //  .AddScript("~/Scripts/Second.jsx");
+
+                // If you use an external build too (for example, Babel, Webpack,
+                // Browserify or Gulp), you can improve performance by disabling
+                // ReactJS.NET's version of Babel and loading the pre-transpiled
+                // scripts. Example:
+                //config
+                //  .SetLoadBabel(false)
+                //  .AddScriptWithoutTransform("~/Scripts/bundle.server.js");
+            });
 
             app.UseAuthentication();
             app.UseDeveloperExceptionPage();
