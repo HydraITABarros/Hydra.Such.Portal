1ab8994666c3ad04eebaee9badca6e207bf57484
andre bastos <andre.bastos@geratriz.pt>
2019-04-17 22:04:51 +0100

    Add react styleguide to generate automated component documentation

---
+++ Hydra.Such.Portal/Hydra.Such.Portal/styleguide/components/StyleGuide.js
@@ -0,0 +1,91 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+import Logo from 'rsg-components/Logo';
+import Markdown from 'rsg-components/Markdown';
+import Styled from 'rsg-components/Styled';
+
+const xsmall = '@media (max-width: 600px)';
+
+const styles = ({ font, base, light, link, baseBackground, mq, space, fontFamily }) => ({
+    root: {
+        color: base,
+        backgroundColor: baseBackground,
+    },
+    header: {
+        color: '#fff',
+        backgroundColor: link,
+    },
+    bar: {
+        display: 'flex',
+        alignItems: 'center',
+        [xsmall]: {
+            flexDirection: 'column',
+            alignItems: 'center',
+        },
+    },
+    nav: {
+        marginLeft: 'auto',
+        marginRight: '-0.5em',
+        [xsmall]: {
+            margin: [[10, 0, 0]],
+        },
+    },
+    headerLink: {
+        '&, &:link, &:visited': {
+            marginLeft: '0.5em',
+            marginRight: '0.5em',
+            fontFamily: font,
+            color: '#efefef',
+        },
+        '&:hover, &:active': {
+            color: '#fff',
+            cursor: 'pointer',
+        },
+    },
+    content: {
+        maxWidth: 1000,
+        padding: [[15, 30]],
+        margin: [[0, 'auto']],
+        [mq.small]: {
+            padding: 15,
+        },
+        display: 'block',
+    },
+    components: {
+        overflow: 'auto', // To prevent the pane from growing out of the screen
+    },
+    footer: {
+        display: 'block',
+        color: light,
+        fontFamily: font,
+        fontSize: 12,
+    },
+    descriptionText: {
+        marginTop: space[0],
+        fontFamily: fontFamily.base,
+    },
+});
+
+export function StyleGuideRenderer({ classes, title, homepageUrl, children }) {
+    children.title = title;
+    return (
+        <div className={classes.root}>
+
+            <main className={classes.content}>
+                {children}
+                <footer className={classes.footer}>
+                    <Markdown text={`Created with [React Styleguidist](${homepageUrl}) ❤️`} />
+                </footer>
+            </main>
+        </div>
+    );
+}
+
+StyleGuideRenderer.propTypes = {
+    classes: PropTypes.object.isRequired,
+    title: PropTypes.string.isRequired,
+    homepageUrl: PropTypes.string.isRequired,
+    children: PropTypes.node.isRequired,
+};
+
+export default Styled(styles)(StyleGuideRenderer);
