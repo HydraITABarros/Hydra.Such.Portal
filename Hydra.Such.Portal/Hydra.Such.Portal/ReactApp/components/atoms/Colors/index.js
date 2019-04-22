7bd10cb66669e956485e8f0eb24515a6e9b28146
andre bastos <andre.bastos@geratriz.pt>
2019-04-23 00:31:05 +0100

    Update colors component remove class attribute

--- Hydra.Such.Portal/Hydra.Such.Portal/ReactApp/components/atoms/Colors/index.js
+++ Hydra.Such.Portal/Hydra.Such.Portal/ReactApp/components/atoms/Colors/index.js
@@ -19,89 +19,89 @@ const ColorBox = styled.div`
         width: 60px;
         height: 60px;
         background: ${props => props.color};
-        border: ${props => (props.border||0)+"px"} solid #CCCCCC;
+        border: ${props => (props.border || 0) + "px"} solid #CCCCCC;
     }
 `
 const Colors = ({ type, ...props }) => {
-console.log(_theme);
-    
-    return(
-    <div>   
-        <Wrapper>
-            <ColorBox>
-                <span class="rsg--para-11">Primary</span>
-            </ColorBox>
-            <ColorBox color={_theme.palette.primary.default} >
-                <span class="rsg--para-11">default</span>
-            </ColorBox>
-            <ColorBox color={_theme.palette.primary.dark} >
-                <span class="rsg--para-11">dark</span>
-            </ColorBox>
-            <ColorBox color={_theme.palette.primary.medium} >
-                <span class="rsg--para-11">medium</span>
-            </ColorBox>
-            <ColorBox color={_theme.palette.primary.light} >
-                <span class="rsg--para-11">light</span>
-            </ColorBox>
-            <ColorBox color={_theme.palette.primary.keylines} >
-                <span class="rsg--para-11">keylines</span>
-            </ColorBox>
-        </Wrapper>
+    console.log(_theme);
 
-        <Wrapper>
-            <ColorBox>
-                <span class="rsg--para-11">Secondary</span>
-            </ColorBox>
-            <ColorBox color={_theme.palette.secondary.default} >
-                <span class="rsg--para-11">default</span>
-            </ColorBox>
-            <ColorBox color={_theme.palette.secondary.light} >
-                <span class="rsg--para-11">light</span>
-            </ColorBox>
-        </Wrapper>
+    return (
+        <div>
+            <Wrapper>
+                <ColorBox>
+                    <span className="rsg--para-11">Primary</span>
+                </ColorBox>
+                <ColorBox color={_theme.palette.primary.default} >
+                    <span className="rsg--para-11">default</span>
+                </ColorBox>
+                <ColorBox color={_theme.palette.primary.dark} >
+                    <span className="rsg--para-11">dark</span>
+                </ColorBox>
+                <ColorBox color={_theme.palette.primary.medium} >
+                    <span className="rsg--para-11">medium</span>
+                </ColorBox>
+                <ColorBox color={_theme.palette.primary.light} >
+                    <span className="rsg--para-11">light</span>
+                </ColorBox>
+                <ColorBox color={_theme.palette.primary.keylines} >
+                    <span className="rsg--para-11">keylines</span>
+                </ColorBox>
+            </Wrapper>
 
-        <Wrapper>
-            <ColorBox>
-                <span class="rsg--para-11">Background</span>
-            </ColorBox>
-            <ColorBox color={_theme.palette.bg.white} border="1">
-                <span class="rsg--para-11">white</span>
-            </ColorBox>
-            <ColorBox color={_theme.palette.bg.grey} >
-                <span class="rsg--para-11">grey</span>
-            </ColorBox>
-        </Wrapper>
+            <Wrapper>
+                <ColorBox>
+                    <span className="rsg--para-11">Secondary</span>
+                </ColorBox>
+                <ColorBox color={_theme.palette.secondary.default} >
+                    <span className="rsg--para-11">default</span>
+                </ColorBox>
+                <ColorBox color={_theme.palette.secondary.light} >
+                    <span className="rsg--para-11">light</span>
+                </ColorBox>
+            </Wrapper>
 
-        <Wrapper>
-            <ColorBox>
-                <span class="rsg--para-11">Alert</span>
-            </ColorBox>
-            <ColorBox color={_theme.palette.alert.bad} >
-                <span class="rsg--para-11">bad</span>
-            </ColorBox>
-            <ColorBox color={_theme.palette.alert.good} >
-                <span class="rsg--para-11">good</span>
-            </ColorBox>
-        </Wrapper>
+            <Wrapper>
+                <ColorBox>
+                    <span className="rsg--para-11">Background</span>
+                </ColorBox>
+                <ColorBox color={_theme.palette.bg.white} border="1">
+                    <span className="rsg--para-11">white</span>
+                </ColorBox>
+                <ColorBox color={_theme.palette.bg.grey} >
+                    <span className="rsg--para-11">grey</span>
+                </ColorBox>
+            </Wrapper>
 
-        <Wrapper>
-            <ColorBox>
-                <span class="rsg--para-11">Search</span>
-            </ColorBox>
-            <ColorBox color={_theme.palette.search} >
-                <span class="rsg--para-11">search</span>
-            </ColorBox>
-        </Wrapper>
+            <Wrapper>
+                <ColorBox>
+                    <span className="rsg--para-11">Alert</span>
+                </ColorBox>
+                <ColorBox color={_theme.palette.alert.bad} >
+                    <span className="rsg--para-11">bad</span>
+                </ColorBox>
+                <ColorBox color={_theme.palette.alert.good} >
+                    <span className="rsg--para-11">good</span>
+                </ColorBox>
+            </Wrapper>
 
-        <Wrapper>
-            <ColorBox>
-                <span class="rsg--para-11">White</span>
-            </ColorBox>
-            <ColorBox  color={_theme.palette.bg.white} border="1">
-                <span class="rsg--para-11">white</span>
-            </ColorBox>
-        </Wrapper>
-    </div>
+            <Wrapper>
+                <ColorBox>
+                    <span className="rsg--para-11">Search</span>
+                </ColorBox>
+                <ColorBox color={_theme.palette.search} >
+                    <span className="rsg--para-11">search</span>
+                </ColorBox>
+            </Wrapper>
+
+            <Wrapper>
+                <ColorBox>
+                    <span className="rsg--para-11">White</span>
+                </ColorBox>
+                <ColorBox color={_theme.palette.bg.white} border="1">
+                    <span className="rsg--para-11">white</span>
+                </ColorBox>
+            </Wrapper>
+        </div>
     )
 }
 
