cea34e4a58e3b6516945b8f9490a7ea382974d3a
Vasco Alves <vasco.alves@geratriz.com>
2019-04-26 19:23:39 +0100

    Created Modal component files (empty)

---
+++ Hydra.Such.Portal/Hydra.Such.Portal/ReactApp/components/atoms/Modal/index.js
@@ -0,0 +1,11 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+import styled, { css, theme } from 'styled-components';
+import _theme from '../../themes/default';
+import {Text} from 'components';
+
+
+const Modal = ({ type, ...props }) => {
+}
+
+export default Modal;
