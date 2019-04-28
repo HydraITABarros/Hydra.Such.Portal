d65b4ebd843a79916cd42faa347f962830554f91
andre bastos <andre.bastos@geratriz.pt>
2019-04-28 23:50:53 +0100

    Updates component Button removes unused if statements

--- Hydra.Such.Portal/Hydra.Such.Portal/ReactApp/components/atoms/Button/index.js
+++ Hydra.Such.Portal/Hydra.Such.Portal/ReactApp/components/atoms/Button/index.js
@@ -1,9 +1,8 @@
 import React from 'react';
-import PropTypes from 'prop-types';
 import styled, { css, theme } from 'styled-components';
 import MuiButton from '@material-ui/core/Button';
 import _theme from '../../themes/default';
-import {Text} from 'components';
+import { Text } from 'components';
 import Color from 'color';
 
 const buttonTextPadding = {
@@ -64,8 +63,8 @@ const buttonRound = css`&& {
     background-color: ${_theme.palette.white};
     color: ${_theme.palette.primary.medium};
     b {
-    color: ${_theme.palette.primary.medium};
-    line-height: 14px;
+        color: ${_theme.palette.primary.medium};
+        line-height: 14px;
     }
 }
 `
@@ -79,7 +78,7 @@ const buttonOutline = css`&& {
     border-radius: ${_theme.radius.primary};
     b {
         color: ${_theme.palette.secondary.default};
-        }
+    }
 }
 `
 const ButtonPrimary = styled(MuiButton)`${sharedStyles}${buttonPrimary}`;
@@ -88,37 +87,21 @@ const ButtonIcon = styled(MuiButton)`${sharedStyles}${buttonIcon}`;
 const ButtonRound = styled(MuiButton)`${buttonRound}`;
 const ButtonOutline = styled(MuiButton)`${buttonOutline}`;
 
-
-
-const Button = ({ type, ...props }) => {
-    
-    const { to, href } = props
+const Button = ({ ...props }) => {
     if (props.primary) {
         return <ButtonPrimary variant="contained" color="primary" {...props} ><Text b>{props.children}</Text></ButtonPrimary>
-    } if (href) {
-        return <ButtonPrimary {...props} />
     } else if (props.default) {
         return <ButtonDefault variant="contained" {...props} ><Text b>{props.children}</Text></ButtonDefault>
-    } if (href) {
-        return <ButtonDefault {...props} />
     } else if (props.icon) {
         return <ButtonIcon variant="contained" {...props} >{props.icon}{"\u00a0"}<Text b>{props.children}</Text></ButtonIcon>
-    } if (href) {
-        return <ButtonIcon {...props} />
     } else if (props.link) {
         return <Link src="javascript:void(0)" {...props} ><Text b>{props.children}</Text></Link>
-    } if (href) {
-        return <Link {...props} />
     } else if (props.round) {
         return <ButtonRound src="javascript:void(0)" {...props} ></ButtonRound>
-    } if (href) {
-        return <ButtonRound {...props} />
     } else if (props.outline) {
         return <ButtonOutline variant="contained" {...props} ><Text b>{props.children}</Text></ButtonOutline>
-    } if (href) {
-        return <ButtonOutline {...props} />
     }
-    return <ButtonPrimary {...props} type={type} />
+    return <ButtonPrimary {...props} />
 }
 
 export default Button;
