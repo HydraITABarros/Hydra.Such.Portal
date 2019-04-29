f4121ad56c85975817ad35c871e920a2e7acccee
andre bastos <andre.bastos@geratriz.pt>
2019-04-29 18:30:01 +0100

    Update Modal component adds model wrapper and basic functionalities

--- Hydra.Such.Portal/Hydra.Such.Portal/ReactApp/components/atoms/Modal/Readme.md
+++ Hydra.Such.Portal/Hydra.Such.Portal/ReactApp/components/atoms/Modal/Readme.md
@@ -1,5 +1,36 @@
 ```jsx
+import Button from '../Button';
+import Text from '../Text';
+import {Modal, DialogTitle,DialogContent,DialogActions} from './';
 
+const Content = (
+    <div>
+        <DialogTitle>Modal title </DialogTitle>
+        <DialogContent>
+            <Text p>
+                Cras mattis consectetur purus sit amet fermentum. Cras justo odio, dapibus ac
+                facilisis in, egestas eget quam. Morbi leo risus, porta ac consectetur ac, vestibulum
+                at eros.
+            </Text>
+            <Text p>
+                Praesent commodo cursus magna, vel scelerisque nisl consectetur et. Vivamus sagittis
+                lacus vel augue laoreet rutrum faucibus dolor auctor.
+            </Text>
+            <Text p>
+                Aenean lacinia bibendum nulla sed consectetur. Praesent commodo cursus magna, vel
+                scelerisque nisl consectetur et. Donec sed odio dui. Donec ullamcorper nulla non metus
+                auctor fringilla.
+            </Text>
+        </DialogContent>
+        <DialogActions>
+            <Button color="primary">Save changes</Button>
+        </DialogActions>
+    </div>
+);
 
-
+<Modal action={
+    <Button primary >Open Modal</Button>
+    }>
+    {Content}
+</Modal>
 ```
--- Hydra.Such.Portal/Hydra.Such.Portal/ReactApp/components/atoms/Modal/index.js
+++ Hydra.Such.Portal/Hydra.Such.Portal/ReactApp/components/atoms/Modal/index.js
@@ -2,10 +2,67 @@ import React from 'react';
 import PropTypes from 'prop-types';
 import styled, { css, theme } from 'styled-components';
 import _theme from '../../themes/default';
-import {Text} from 'components';
+import { Text } from 'components';
+import Dialog from '@material-ui/core/Dialog';
+import MuiDialogTitle from '@material-ui/core/DialogTitle';
+import MuiDialogContent from '@material-ui/core/DialogContent';
+import MuiDialogActions from '@material-ui/core/DialogActions';
 
+const ActionDiv = styled.div`
+    display: inline-block;
+`;
 
-const Modal = ({ type, ...props }) => {
+const Action = ({ ...props }) => {
+    return <ActionDiv {...props}>{props.children}</ActionDiv>
 }
 
-export default Modal;
+class Modal extends React.Component {
+    state = {
+        open: false,
+    }
+
+    constructor(props) {
+        super(props);
+        this.handleClickOpen = this.handleClickOpen.bind(this);
+    }
+
+    handleClickOpen = () => {
+        console.log(23423423);
+        this.setState({
+            open: true,
+        });
+    };
+
+    handleClose = () => {
+        this.setState({ open: false });
+    };
+
+    render() {
+        return (
+            <div>
+                <Action onClick={this.handleClickOpen} >
+                    {this.props.action}
+                </Action>
+                <Dialog
+                    onClose={this.handleClose}
+                    aria-labelledby="customized-dialog-title"
+                    open={this.state.open}
+                >
+                    {this.props.children}
+                </Dialog>
+            </div>
+
+        );
+
+    }
+}
+const DialogTitle = MuiDialogTitle;
+const DialogContent = MuiDialogContent;
+const DialogActions = MuiDialogActions;
+
+export {
+    Modal,
+    DialogTitle,
+    DialogContent,
+    DialogActions
+};
