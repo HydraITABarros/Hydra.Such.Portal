f880564da9d36dfba8cebe2382b777b9fe286bba
Vasco Alves <vasco.alves@geratriz.com>
2019-04-29 16:33:33 +0100

    Added Avatars component

---
+++ Hydra.Such.Portal/Hydra.Such.Portal/ReactApp/components/atoms/Avatars/Readme.md
@@ -0,0 +1,24 @@
+```jsx
+
+<div>
+    <Avatars.Avatars photo src="https://material-ui.com/static/images/avatar/1.jpg" />{"\u00a0","\u00a0","\u00a0"}
+    <Avatars.Avatars letter color="#0033FF">VA</Avatars.Avatars>{"\u00a0","\u00a0","\u00a0"}
+
+    <Avatars.AvatarGroup>
+        <Avatars.Avatars letter color="#990000">AB</Avatars.Avatars>
+        <Avatars.Avatars letter color="#33DDEE">SB</Avatars.Avatars>
+        <Avatars.Avatars letter color="#5533DD">GP</Avatars.Avatars>
+        <Avatars.Avatars letter color="#339900">CA</Avatars.Avatars>
+        <Avatars.Avatars letter color="#cc00cc">SF</Avatars.Avatars>
+    </Avatars.AvatarGroup>{"\u00a0","\u00a0","\u00a0"}
+
+    <Avatars.AvatarGroup>
+        <Avatars.Avatars photo src="https://material-ui.com/static/images/avatar/1.jpg" />
+        <Avatars.Avatars photo src="https://material-ui.com/static/images/avatar/1.jpg" />
+        <Avatars.Avatars photo src="https://material-ui.com/static/images/avatar/1.jpg" />
+        <Avatars.Avatars photo src="https://material-ui.com/static/images/avatar/1.jpg" />
+        <Avatars.Avatars photo src="https://material-ui.com/static/images/avatar/1.jpg" />
+    </Avatars.AvatarGroup>
+</div>
+
+```
---
+++ Hydra.Such.Portal/Hydra.Such.Portal/ReactApp/components/atoms/Avatars/index.js
@@ -0,0 +1,52 @@
+import React from 'react';
+import styled, { css, theme } from 'styled-components';
+import _theme from '../../themes/default';
+import Color from 'color';
+import { Text } from 'components';
+import { Button } from 'components';
+
+const avatarPhoto = css`&& {
+    display: inline-block;
+    overflow: hidden;
+    img {
+        width: 40px;        
+    }
+}
+`
+const avatarLetter = css`&& {
+    display: inline-block;
+    background-color: ${props => props.color};
+    h3 {    
+        margin: 0;
+        line-height: 18px;
+        font-weight: 400;
+        color: ${_theme.palette.white};
+    }
+}
+`
+const AvatarGroup = styled.div` 
+    display: inline-block;
+    > Button {
+        margin-left: -1.5em;
+        &:first-child {
+            margin-left: 0;
+        }
+    }
+`
+
+const AvatarLetter = styled(Button)`${avatarLetter}`;
+const AvatarPhoto = styled(Button)`${avatarPhoto}`;
+
+const Avatars = ({ ...props }) => {
+    if (props.photo) {
+        return <AvatarPhoto round {...props} ><img src={props.src}>{props.children}</img></AvatarPhoto>
+    } else if (props.letter) {
+        return <AvatarLetter round {...props} ><Text h3>{props.children}</Text></AvatarLetter>
+    }
+    return <AvatarPhoto {...props} />
+}
+
+// export default Avatars;
+export {
+    Avatars, AvatarGroup
+}
