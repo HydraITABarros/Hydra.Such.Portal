55c9faeb8a2e81d09f477c5f0731bd79d9d7dec0
andre bastos <andre.bastos@geratriz.pt>
2019-04-29 18:24:53 +0100

    Update icons on Checkbox component

--- Hydra.Such.Portal/Hydra.Such.Portal/ReactApp/components/atoms/CheckBox/index.js
+++ Hydra.Such.Portal/Hydra.Such.Portal/ReactApp/components/atoms/CheckBox/index.js
@@ -2,6 +2,7 @@ import React from 'react'
 import styled, { css, theme } from 'styled-components'
 import _theme from '../../themes/default';
 import MuiCheckbox from '@material-ui/core/Checkbox';
+import { Icon } from 'components';
 
 const styles = css`&& {
     }
@@ -11,7 +12,7 @@ const DefaultCheckBox = styled(MuiCheckbox)`${styles}`;
 
 const CheckBox = ({ ...props }) => {
 
-    return <DefaultCheckBox {...props} />
+    return <DefaultCheckBox checkedIcon={<Icon validation />} icon={<Icon warning />} {...props} />
 }
 
 export default CheckBox;
