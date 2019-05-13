ba926a394f86195e9c13966ba2b9c793eeb99809
andre bastos <andre.bastos@geratriz.pt>
2019-05-13 10:58:53 +0100

    Update React components import

--- Hydra.Such.Portal/Hydra.Such.Portal/ReactApp/components/index.js
+++ Hydra.Such.Portal/Hydra.Such.Portal/ReactApp/components/index.js
@@ -2,6 +2,11 @@
 const req = require.context('.', true, /\.\/[^/]+\/[^/]+\/index\.js$/)
 
 req.keys().forEach((key) => {
-    const componentName = key.replace(/^.+\/([^/]+)\/index\.js/, '$1')
-    module.exports[componentName] = req(key).default
+    const componentName = key.replace(/^.+\/([^/]+)\/index\.js/, '$1');
+    
+    if (req(key).default) {
+        module.exports[componentName] = req(key).default;
+    } else {
+        module.exports[componentName] = req(key);
+    }
 })
