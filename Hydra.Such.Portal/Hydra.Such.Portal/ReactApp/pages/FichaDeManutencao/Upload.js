cad9bdec35fa5c4686d80c159f9c530cfc8d1b74
Vasco Alves <vasco.alves@geratriz.com>
2019-07-15 18:48:03 +0100

    Updated Modal Tabs with GSelect compontent

--- Hydra.Such.Portal/Hydra.Such.Portal/ReactApp/pages/FichaDeManutencao/Upload.js
+++ Hydra.Such.Portal/Hydra.Such.Portal/ReactApp/pages/FichaDeManutencao/Upload.js
@@ -1,8 +1,9 @@
 import React, { Component } from 'react';
-import { Input, Button, Icon, Text, Wrapper, Select, MenuItem } from 'components';
+import { Input, Button, Icon, Text, Wrapper, Select, MenuItem, GSelect } from 'components';
 import MuiAddIcon from '@material-ui/icons/Add';
 import styled, { css, theme, injectGlobal, withTheme } from 'styled-components';
 import MuiGrid from '@material-ui/core/Grid';
+
 
 const Grid = styled(MuiGrid)`
     position: relative;
@@ -10,6 +11,7 @@ const Grid = styled(MuiGrid)`
 const TextUpload = styled(Text)`
     margin-left: 16px;
 `
+
 const Upload = (props) => {
   return (
     <div>      
@@ -17,15 +19,17 @@ const Upload = (props) => {
       <Wrapper padding={'0 0 16px'}>
         <Grid container spacing={16}>
           <Grid item xs={10} md={5}>
-          <Select>
-              <MenuItem value="">#1 236465</MenuItem>
-              <MenuItem value={10}>#2 321354</MenuItem>
-              <MenuItem value={20}>#3 354544</MenuItem>
-              <MenuItem value={30}>#4 654321</MenuItem>
-          </Select>
+          <GSelect           
+            placeholder=
+              {"Associar a equipamento"}
+            options={[
+              {value:10, title: "#1 19283012"}, 
+              {value:20, title: "#2 19283012"},
+              ]}
+            />
           </Grid>
           <Grid item xs={10}>
-              <Button iconPrimary={<Icon download/>}>Upload</Button><TextUpload>Até 100MB</TextUpload>
+              <Button iconPrimary={<Icon upload/>}>Upload</Button><TextUpload>Até 100MB</TextUpload>
           </Grid>
         </Grid>
       </Wrapper>
