9232cb642f085d018c90105addbf2c01d23afca4
Vasco Alves <vasco.alves@geratriz.com>
2019-07-15 18:42:07 +0100

    New Molecule: GSelect (Select with placeholder + value options)

---
+++ Hydra.Such.Portal/Hydra.Such.Portal/ReactApp/components/molecules/GSelect/index.js
@@ -0,0 +1,28 @@
+import React from 'react';
+import styled, { css, theme } from 'styled-components';
+import { Input, Button, Icon, Text, Wrapper, Select, MenuItem } from 'components';
+
+const Span = styled.span` 
+    color: ${props => props.theme.palette.primary.medium};
+`
+
+const GSelect = (props) => {
+
+      return (
+        <div>
+          <Select value >
+          {props.nullable&&
+            <MenuItem value><em>None</em></MenuItem>
+          }
+          {props.placeholder&&
+            <MenuItem value style={{display: 'none'}}><Span>{props.placeholder}</Span></MenuItem>
+          }
+              {props.options&&props.options.map(item => {
+                return <MenuItem value={item.value}>{item.title}</MenuItem>
+              } )}
+          </Select> 
+        </div>
+    )
+  }
+
+export default GSelect;
