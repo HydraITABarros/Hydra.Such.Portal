acf23ffa9d2834e87a8d35aad0986c4a4345cc90
sarabastos <sara.bastos@geratriz.pt>
2019-07-16 15:09:09 +0100

    Updated EquipmentViewModel

--- Hydra.Such.Portal/Hydra.Such.Portal/ViewModels/EquipamentoViewModel.cs
+++ Hydra.Such.Portal/Hydra.Such.Portal/ViewModels/EquipamentoViewModel.cs
@@ -1,4 +1,5 @@
-﻿using Microsoft.AspNet.OData.Query;
+﻿using Hydra.Such.Data.Evolution.DatabaseReference;
+using Microsoft.AspNet.OData.Query;
 using Microsoft.AspNetCore.Mvc;
 using Newtonsoft.Json;
 using Newtonsoft.Json.Converters;
@@ -10,7 +11,7 @@ using System.ComponentModel.DataAnnotations;
 using System.ComponentModel.DataAnnotations.Schema;
 using System.Runtime.Serialization;
 
-namespace Hydra.Such.Data.Evolution.DatabaseReference
+namespace Hydra.Such.Portal.ViewModels
 {
 
     public partial class EquipamentoViewModel
@@ -31,50 +32,55 @@ namespace Hydra.Such.Data.Evolution.DatabaseReference
         public string haveCurative { get; set; }
 
 
+        [NotMapped]
+        public string ModeloText { get; set; }
+
+        [NotMapped]
+        public List<EquipMarca> Marcas;
+
+        [NotMapped]
+        public bool iconTaskMaintenance { get; set; }
+
+        [NotMapped]
+        public bool iconTaskQuality { get; set; }
+
+        [NotMapped]
+        public bool iconTaskQuantity { get; set; }
+
+        [NotMapped]
+        public int? iconTaskEMM { get; set; }
+
+
+
+
+
 
 
         [Key]
         public int IdEquipamento { get; set; }
-
         public string Nome { get; set; }
-
         public int Marca { get; set; }
-
         public int Modelo { get; set; }
-
         public int Categoria { get; set; }
-
-
         public string NumSerie { get; set; }
-
         public string NumInventario { get; set; }
-
         public string NumEquipamento { get; set; }
-
         public string Observacao { get; set; }
         public string IdFornecedor { get; set; }
         public string NomeFornecedor { get; set; }
-
         public int IdCliente { get; set; }
-
         public int IdServico { get; set; }
-
         public bool? Activo { get; set; }
         public DateTime? DataAquisicao { get; set; }
         public DateTime? DataInstalacao { get; set; }
         public int? AnoFabrico { get; set; }
         public int? IdRegiao { get; set; }
         public int? IdArea { get; set; }
-
         public int? IdEquipa { get; set; }
         public int? IdAreaOp { get; set; }
-
         public string IdContrato { get; set; }
-
         public byte[] CodigoBarras { get; set; }
-
         public byte[] CodigoBarrasCliente { get; set; }
-
         public decimal? PrecoAquisicao { get; set; }
         public string Localizacao { get; set; }
         public int? IdPeriodicidade { get; set; }
@@ -87,9 +93,7 @@ namespace Hydra.Such.Data.Evolution.DatabaseReference
         public DateTime? DataEntradaContrato { get; set; }
         public DateTime? DataSaidaContrato { get; set; }
         public string DesignacaoComplementar { get; set; }
-
         public byte[] Foto { get; set; }
-
         public int? AlteradoPor { get; set; }
         public DateTime? DataAlteracao { get; set; }
         public int ToleranciaEquipamento { get; set; }
@@ -104,7 +108,7 @@ namespace Hydra.Such.Data.Evolution.DatabaseReference
         public string Sala { get; set; }
         public string DesignacaoComplementar2 { get; set; }
         public bool? SubContratar { get; set; }
-
     }
+
 
 }
