862a146c8b261518eb14f76226e3de67d9774a81
andre bastos <andre.bastos@geratriz.pt>
2019-07-17 16:14:52 +0100

    Adds helper functions file

---
+++ Hydra.Such.Portal/Hydra.Such.Portal/ReactApp/helpers/functions.js
@@ -0,0 +1,18 @@
+export default {
+	toTitleCase: (str) => {
+		if (!str) {
+			return str;
+		}
+		return str.replace(
+			/\w\S*/g,
+			function (txt) {
+				return txt.charAt(0).toUpperCase() + txt.substr(1).toLowerCase();
+			}
+		);
+	},
+	getInitials(name) {
+		var initials = name.match(/\b\w/g) || [];
+		initials = ((initials.shift() || '') + (initials.pop() || '')).toUpperCase();
+		return initials;
+	}
+}
