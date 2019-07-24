5aa1165826b436a50474b46f35c086162f03d59e
andre bastos <andre.bastos@geratriz.pt>
2019-07-24 14:53:30 +0100

    Update react app.js adds ficha de manutencao route

--- Hydra.Such.Portal/Hydra.Such.Portal/ReactApp/App.js
+++ Hydra.Such.Portal/Hydra.Such.Portal/ReactApp/App.js
@@ -2,7 +2,7 @@
 import { Switch, Route } from 'react-router-dom'
 import { injectGlobal, ThemeProvider } from 'styled-components'
 
-import { HomePage, SamplePage, NotFoundPage, Template, OrdensDeManutencao, OrdensDeManutencaoDetails, Arquivo } from 'pages'
+import { HomePage, SamplePage, NotFoundPage, Template, OrdensDeManutencao, OrdensDeManutencaoDetails, FichaDeManutencao, Arquivo } from 'pages'
 
 // https://github.com/diegohaz/arc/wiki/Styling
 import theme from './themes/default'
@@ -13,14 +13,14 @@ import { Provider } from 'react-redux';
 import store from './store';
 
 const muiTheme = createMuiTheme({
-        palette: {
-                action: {
-                        selected: Color(theme.palette.primary.keylines).rgb().fade(0.1).toString(),
-                        hover: Color(theme.palette.primary.keylines).rgb().fade(0.5).toString(),
-                        active: Color(theme.palette.primary.ligkeylinesht).rgb().fade(0.5).toString(),
-                        disabled: Color(theme.palette.primary.keylines).rgb().fade(0.8).toString(),
-                }
-        }
+	palette: {
+		action: {
+			selected: Color(theme.palette.primary.keylines).rgb().fade(0.1).toString(),
+			hover: Color(theme.palette.primary.keylines).rgb().fade(0.5).toString(),
+			active: Color(theme.palette.primary.ligkeylinesht).rgb().fade(0.5).toString(),
+			disabled: Color(theme.palette.primary.keylines).rgb().fade(0.8).toString(),
+		}
+	}
 });
 
 injectGlobal`
@@ -30,23 +30,23 @@ injectGlobal`
 `
 
 const App = () => {
-        return (
-                <Provider store={store}>
-                        <ThemeProvider theme={theme}>
-                                <MuiThemeProvider theme={muiTheme}>
-                                        <Switch>
-                                                {/* <Route path="/" component={HomePage} exact /> */}
-                                                {/* <Route component={Template} /> */}
-                                                <Route path="/ordens-de-manutencao" component={OrdensDeManutencao} exact />
-                                                <Route path="/ordens-de-manutencao/arquivo" component={Arquivo} exact />
-                                                <Route path="/ordens-de-manutencao/:orderid" component={OrdensDeManutencaoDetails} exact />
-                                                <Route path="/ordens-de-manutencao/:orderid/ficha-de-manutencao" render={() => <div></div>} />
-                                                {/* <Route component={NotFoundPage} /> */}
-                                        </Switch>
-                                </MuiThemeProvider>
-                        </ThemeProvider>
-                </Provider>
-        )
+	return (
+		<Provider store={store}>
+			<ThemeProvider theme={theme}>
+				<MuiThemeProvider theme={muiTheme}>
+					<Switch>
+						{/* <Route path="/" component={HomePage} exact /> */}
+						{/* <Route component={Template} /> */}
+						<Route path="/ordens-de-manutencao" component={OrdensDeManutencao} exact />
+						<Route path="/ordens-de-manutencao/arquivo" component={Arquivo} exact />
+						<Route path="/ordens-de-manutencao/:orderid" component={OrdensDeManutencaoDetails} exact />
+						<Route path="/ordens-de-manutencao/:orderid/ficha-de-manutencao" component={FichaDeManutencao} exact />
+						{/* <Route component={NotFoundPage} /> */}
+					</Switch>
+				</MuiThemeProvider>
+			</ThemeProvider>
+		</Provider>
+	)
 }
 
 export default App
