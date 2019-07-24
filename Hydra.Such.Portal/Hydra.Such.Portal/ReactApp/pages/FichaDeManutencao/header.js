65c811208be90d6b793aae1421e49babd1586597
andre bastos <andre.bastos@geratriz.pt>
2019-07-24 20:02:00 +0100

    Update ficha de manutencao header

--- Hydra.Such.Portal/Hydra.Such.Portal/ReactApp/pages/FichaDeManutencao/header.js
+++ Hydra.Such.Portal/Hydra.Such.Portal/ReactApp/pages/FichaDeManutencao/header.js
@@ -7,6 +7,7 @@ import styled, { css, theme, injectGlobal, withTheme } from 'styled-components';
 import { createMuiTheme } from '@material-ui/core/styles';
 import Hidden from '@material-ui/core/Hidden';
 import { withRouter } from 'react-router-dom';
+
 const muiTheme = createMuiTheme();
 const breakpoints = muiTheme.breakpoints.values;
 injectGlobal`
@@ -16,11 +17,16 @@ const CustomWrapper = styled(Wrapper)` && {
 	border-bottom: solid 1px ${props => props.theme.palette.primary.keylines};
 }`
 
+const Grid = styled(MuiGrid)`&& {
+	padding: 20px 25px;
+       white-space: nowrap;
+       overflow: hidden;
+       text-overflow: ellipsis;
+}`
 
 class Header extends Component {
 	state = {
 		isLoading: true,
-
 		orderId: null,
 		technicals: null,
 		service: null,
@@ -36,29 +42,34 @@ class Header extends Component {
 
 	componentDidUpdate(props) {
 		var newState = {};
+
+
 		if (props.isLoading !== this.state.isLoading) {
 			newState.isLoading = props.isLoading;
 		}
 		if (props.orderId !== this.state.orderId) {
-			newState.orderId = this.state.orderId;
+			newState.orderId = props.orderId;
 		}
 		if (props.technicals !== this.state.technicals) {
-			newState.technicals = this.state.technicals;
+			newState.technicals = props.technicals;
 		}
 		if (props.service !== this.state.service) {
-			newState.service = this.state.service;
+			newState.service = props.service;
 		}
 		if (props.room !== this.state.room) {
-			newState.room = this.state.room;
+			newState.room = props.room;
 		}
 		if (props.title !== this.state.title) {
-			newState.title = this.state.title;
+			newState.title = props.title;
 		}
 		if (props.equipments !== this.state.equipments) {
-			newState.equipments = this.state.equipments;
+			newState.equipments = props.equipments;
 		}
-		if (Object.keys(newState) > 0) {
-			this.setState({ ...newState });
+
+		console.log('IMP', Object.keys(newState), newState);
+
+		if (Object.keys(newState).length > 0) {
+			this.setState(newState, () => console.log(this.state));
 		}
 	}
 
@@ -66,7 +77,41 @@ class Header extends Component {
 		return (
 			<CustomWrapper width="100%" >
 				<MuiGrid container direction="row" justify="space-between" alignitems="middle" spacing={0} maxwidth={'100%'} margin={0} style={{ background: this.props.theme.palette.bg.grey }}>
+					<Grid item xs={12} sm={6} style={{ padding: 0 }}>
 
+						<Text h1>{this.state.title}</Text>
+						<MuiGrid container direction="row" justify="space-between" alignitems="middle" spacing={0} maxwidth={'100%'} margin={0} >
+							<Grid item xs={6} style={{ padding: 0 }}>
+								{this.state.room && <Text span>Piso, Sala</Text>}
+							</Grid>
+							<Grid item xs={6} style={{ padding: 0 }}>
+								{this.state.room && <Text b>{this.state.room}</Text>}
+							</Grid>
+
+							<Grid item xs={6} style={{ padding: 0 }}>
+								<Text span>Serviço</Text>
+							</Grid>
+							<Grid item xs={6} style={{ padding: 0 }}>
+								<Text b>{this.state.service}</Text>
+							</Grid>
+
+							<Grid item xs={6} style={{ padding: 0 }}>
+								<Text span>Tipo tarefa</Text>
+							</Grid>
+							<Grid item xs={6} style={{ padding: 0 }}>
+
+							</Grid>
+
+							<Grid item xs={6} style={{ padding: 0 }}>
+								<Text span>Técnicos</Text>
+							</Grid>
+							<Grid item xs={6} style={{ padding: 0 }}>
+
+							</Grid>
+
+						</MuiGrid>
+
+					</Grid>
 				</MuiGrid>
 			</CustomWrapper>
 		)
--- Hydra.Such.Portal/Hydra.Such.Portal/ReactApp/pages/FichaDeManutencao/index.js
+++ Hydra.Such.Portal/Hydra.Such.Portal/ReactApp/pages/FichaDeManutencao/index.js
@@ -10,6 +10,7 @@ import queryString from 'query-string';
 import Breadcrumb from './breadcrumb';
 import Header from './header';
 import { createMuiTheme } from '@material-ui/core/styles';
+import { TestScheduler } from 'rx';
 
 axios.defaults.headers.post['Accept'] = 'application/json';
 axios.defaults.headers.get['Accept'] = 'application/json';
@@ -25,7 +26,12 @@ class FichaDeManutencao extends Component {
 		categoryId: null,
 		equipmentsIds: null,
 		institution: null,
-		client: null
+		client: null,
+		service: null,
+		types: null,
+		technicals: null,
+		equipments: null,
+		room: null
 	}
 
 	constructor(props) {
@@ -54,12 +60,19 @@ class FichaDeManutencao extends Component {
 		axios.get(url, { params }).then((result) => {
 			var data = result.data;
 			console.log(data);
-			if (data.client && data.institution) {
+			if (data.order && data.equipments) {
 
-				this.setState({
-					client: data.client,
-					institution: data.institution
-				});
+				var state = {
+					client: data.order.clientName,
+					institution: data.order.institutionName,
+					title: data.equipments.length > 0 ? data.equipments[0].categoriaText : "",
+					service: data.equipments.length > 0 ? data.equipments[0].servicoText : "",
+					room: data.equipments.length == 1 ? data.equipments[0].sala : "",
+					technicals: data.order.technicals,
+					equipments: data.equipments,
+				}
+
+				this.setState(state);
 			}
 		}).catch(function (error) {
 		}).then(() => {
@@ -73,12 +86,16 @@ class FichaDeManutencao extends Component {
 	}
 
 	render() {
-		console.log(breakpoints);
 		return (
 			<PageTemplate >
 				<Wrapper padding={'0 0 0'} width="100%" minHeight="274px" ref={el => this.highlightWrapper = el}>
 					<Breadcrumb orderId={this.state.orderId} client={this.state.client} institution={this.state.institution} />
-					<Header />
+					<Header
+						title={this.state.title}
+						room={this.state.room}
+						service={this.state.service}
+						types={this.state.types}
+						technicals={this.state.technicals} />
 				</Wrapper>
 			</PageTemplate>
 		)
