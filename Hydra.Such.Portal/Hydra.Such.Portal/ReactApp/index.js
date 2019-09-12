2cddf9011af71cf8987fad04edb736a6b224c040
andre bastos <andre.bastos@geratriz.pt>
2019-09-12 12:47:36 +0100

    Update react app index

--- Hydra.Such.Portal/Hydra.Such.Portal/ReactApp/index.js
+++ Hydra.Such.Portal/Hydra.Such.Portal/ReactApp/index.js
@@ -7,18 +7,26 @@ import { BrowserRouter } from 'react-router-dom'
 import { basename } from 'config'
 import App from 'App'
 
+import { useStrict } from 'mobx';
+import { Provider } from 'mobx-react';
+
+// For easier debugging
+// window._____APP_STATE_____ = stores;
+
+//useStrict(true);
+
 const renderApp = () => (
-        <BrowserRouter basename={basename}>
-                <App />
-        </BrowserRouter>
+	<BrowserRouter basename={basename}>
+		<App />
+	</BrowserRouter>
 )
 
 const root = document.getElementById('basicreactcomponent')
 render(renderApp(), root)
 
 if (module.hot) {
-        module.hot.accept('App', () => {
-                require('App')
-                render(renderApp(), root)
-        })
+	module.hot.accept('App', () => {
+		require('App')
+		render(renderApp(), root)
+	})
 }
