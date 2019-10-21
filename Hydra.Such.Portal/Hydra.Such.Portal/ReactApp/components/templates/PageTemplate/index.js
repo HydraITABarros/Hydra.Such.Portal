67b14abc5205ede59b2f4813e22e072272d60f3b
andre bastos <andre.bastos@geratriz.pt>
2019-10-22 00:02:44 +0100

    Update page template

--- Hydra.Such.Portal/Hydra.Such.Portal/ReactApp/components/templates/PageTemplate/index.js
+++ Hydra.Such.Portal/Hydra.Such.Portal/ReactApp/components/templates/PageTemplate/index.js
@@ -1,8 +1,9 @@
 // https://github.com/diegohaz/arc/wiki/Atomic-Design#templates
-import React from 'react'
+import React, { Component } from 'react'
 import PropTypes from 'prop-types'
 import styled from 'styled-components'
 import { size } from 'styled-theme'
+import { renderComponent } from 'recompose'
 
 const Wrapper = styled.div`
   
@@ -22,24 +23,29 @@ const Footer = styled.footer`
   
 `
 
-const PageTemplate = ({
-    header, hero, sponsor, children, footer,highlight, ...props
-}) => {
-    return (
-        <Wrapper {...props}>
-            {header && <Header>{header}</Header>}
-            <Highlight>{highlight}</Highlight>
-            <Content>{children}</Content>
-            {footer && <Footer>{footer}</Footer>}
-        </Wrapper>
-    )
+class PageTemplate extends Component {
+	constructor(props) {
+		super(props);
+	}
+
+	render() {
+		//var header, hero, sponsor, children, footer, highlight = this.props;
+		return (
+			<Wrapper {...this.props}>
+				{this.props.header && <Header>{this.props.header}</Header>}
+				<Highlight>{this.props.highlight}</Highlight>
+				<Content>{this.props.children}</Content>
+				{this.props.footer && <Footer>{this.props.footer}</Footer>}
+			</Wrapper>
+		)
+	}
 }
 
 PageTemplate.propTypes = {
-    highlight: PropTypes.node,
-    header: PropTypes.node,
-    footer: PropTypes.node,
-    children: PropTypes.any.isRequired,
+	highlight: PropTypes.node,
+	header: PropTypes.node,
+	footer: PropTypes.node,
+	children: PropTypes.any.isRequired,
 }
 
 export default PageTemplate
