9ad7eb9dba70030cc5ecfc0dcaa5e2d35b2fcffa
andre bastos <andre.bastos@geratriz.pt>
2019-11-01 00:27:07 +0000

    Update Evolution DatabaseReference

--- Hydra.Such.Portal/Hydra.Such.Data.Evolution/DatabaseReference/FichaManutencaoRelatorioRelatorio.cs
+++ Hydra.Such.Portal/Hydra.Such.Data.Evolution/DatabaseReference/FichaManutencaoRelatorioRelatorio.cs
@@ -5,14 +5,22 @@ namespace Hydra.Such.Data.Evolution.DatabaseReference
 {
     public partial class FichaManutencaoRelatorioRelatorio
     {
+        public int Id { get; set; }
         public string Om { get; set; }
         public int IdEquipamento { get; set; }
         public string Codigo { get; set; }
         public string Versao { get; set; }
-        public int IdUtilizador { get; set; }
         public DateTime Data { get; set; }
         public string Relatorio { get; set; }
         public int? EstadoFinal { get; set; }
-        public int Id { get; set; }
+        public string Observacao { get; set; }
+        public string AssinaturaTecnico { get; set; }
+        public int? IdAssinaturaTecnico { get; set; }
+        public string AssinaturaCliente { get; set; }
+        public string AssinaturaSie { get; set; }
+        public int? CriadoPor { get; set; }
+        public DateTime? CriadoEm { get; set; }
+        public int? ActualizadoPor { get; set; }
+        public DateTime? ActualizadoEm { get; set; }
     }
 }
