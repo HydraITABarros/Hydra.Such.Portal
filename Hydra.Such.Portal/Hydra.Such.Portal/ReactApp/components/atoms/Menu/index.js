3363ef62646c15d139314c39d7d5d7a34065fcbd
andre bastos <andre.bastos@geratriz.pt>
2019-11-01 00:30:27 +0000

    Update react Menu component

--- Hydra.Such.Portal/Hydra.Such.Portal/ReactApp/components/atoms/Menu/index.js
+++ Hydra.Such.Portal/Hydra.Such.Portal/ReactApp/components/atoms/Menu/index.js
@@ -1,13 +1,20 @@
 import React, { Component } from 'react'
+import ReactDOM from 'react-dom'
 import styled, { css, theme, withTheme } from 'styled-components'
 import MuiCheckbox from '@material-ui/core/Checkbox';
 import _ from 'lodash';
 import { Icon, Button, MenuItem } from 'components';
 import Menu from '@material-ui/core/Menu';
+import MenuList from '@material-ui/core/MenuList';
+import Popper from '@material-ui/core/Popper';
+import ClickAwayListener from '@material-ui/core/ClickAwayListener';
+import Grow from '@material-ui/core/Grow';
+import Fade from '@material-ui/core/Fade';
+import Paper from '@material-ui/core/Paper';
 import './index.scss';
 
-
 var timeout = 0;
+
 class MenuSelect extends Component {
 	state = {
 		anchorEl: null,
@@ -18,6 +25,7 @@ class MenuSelect extends Component {
 	}
 
 	handleClick = event => {
+		event.target.blur();
 		this.setState({ anchorEl: event.currentTarget });
 	};
 
@@ -33,32 +41,58 @@ class MenuSelect extends Component {
 				<span onClick={this.handleClick} >
 					{this.props.action}
 				</span>
-				<Menu
+				<Popper
 					id="simple-menu"
 					anchorEl={anchorEl}
 					open={Boolean(anchorEl)}
 					onClose={this.handleClose}
+					ref={(el) => this.menuEl = el}
+					transition
+					//placement="bottom-start"
+					style={{ zIndex: 1 }}
 				>
-					{this.props.children != null && (
-						this.props.children.count > 1 ?
-							this.props.children.map((item) => {
-								return (React.cloneElement(item, {
-									onClick: () => {
-										item.props.onClick ? item.props.onClick() : null;
-										this.handleClose();
-									}
-								}));
-							})
-							:
-							React.cloneElement(this.props.children, {
-								onClick: () => {
-
-									this.props.children.props.onClick ? this.props.children.props.onClick() : null;
-									this.handleClose();
-								}
-							})
+					{({ TransitionProps, placement }) => (
+						<Grow
+							{...TransitionProps}
+							id="menu-list-grow"
+							style={{
+								transformOrigin: placement === 'bottom' ? 'center top' : 'center bottom'
+							}}
+							timeout={{
+								enter: 400, exit: 180
+							}}
+						>
+							<Paper elevation={8}
+								style={{ position: 'relative', top: '-35px' }}
+							>
+								<ClickAwayListener onClickAway={this.handleClose}>
+									<MenuList
+									>
+										{this.props.children != null && (
+											this.props.children.length > 1 ?
+												this.props.children.map((item, index) => {
+													return (React.cloneElement(item, {
+														key: index,
+														onClick: () => {
+															item.props.onClick ? item.props.onClick() : null;
+															this.handleClose();
+														}
+													}));
+												})
+												:
+												React.cloneElement(this.props.children, {
+													onClick: () => {
+														this.props.children.props.onClick ? this.props.children.props.onClick() : null;
+														this.handleClose();
+													}
+												})
+										)}
+									</MenuList>
+								</ClickAwayListener>
+							</Paper>
+						</Grow>
 					)}
-				</Menu>
+				</Popper>
 			</div>
 		)
 	}
