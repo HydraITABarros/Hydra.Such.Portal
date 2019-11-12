666406f2dcbc5221eaef2d5b2c302c7243d8c3cc
andre bastos <andre.bastos@geratriz.pt>
2019-11-12 19:51:30 +0000

    Update react input change border focus color

--- Hydra.Such.Portal/Hydra.Such.Portal/ReactApp/components/atoms/Input/index.js
+++ Hydra.Such.Portal/Hydra.Such.Portal/ReactApp/components/atoms/Input/index.js
@@ -1,8 +1,8 @@
 import React from 'react'
-import styled, { css, theme, withTheme } from 'styled-components';
+import styled, {css, theme, withTheme} from 'styled-components';
 import MuiOutlinedInput from '@material-ui/core/OutlinedInput';
-import { observable } from 'mobx';
-import { observer } from 'mobx-react';
+import {observable} from 'mobx';
+import {observer} from 'mobx-react';
 
 const styles = css`&& {
         input {
@@ -15,29 +15,29 @@ const styles = css`&& {
 	    background: ${props => props.theme.palette.bg.white};
         }
         fieldset {
-            border-radius: ${props => { return props.theme.radius.primary }};
+            border-radius: ${props => props.theme.radius.primary};
         }
-	&[class*="MuiOutlinedInput-disabled"] {
-		cursor: not-allowed;
-		* {
+		&.MuiOutlinedInput-root.Mui-disabled {
 			cursor: not-allowed;
-		}
-		fieldset {
-    			border-color: rgba(0, 0, 0, 0.23) ;
-			border-color:${props => props.theme.palette.primary.light};
+			* {
+				cursor: not-allowed;
+			}
+			fieldset {
+				border-color: rgba(0, 0, 0, 0.23) ;
+				border-color:${props => props.theme.palette.primary.light};
         	}
-	}
+		}
         [role="button"] {
             padding: 11px 35px 11px 15px;
         }
-        &[class*="MuiInputBase-focused"] {
+        &.Mui-focused {
             background-color: white;
             [class*="MuiSelect-select"] {
                 background-color: transparent;
             }
             fieldset {
                 border-color: ${props => props.theme.palette.secondary.default};
-		border-width: 1px;
+				border-width: 1px;
             }
         }
     }
@@ -46,19 +46,20 @@ const styles = css`&& {
 const DefaultOutlinedInput = styled(MuiOutlinedInput)`${styles}`;
 var timeout = 0;
 
-const Input = ({ ...props }) => {
-	if (props.$value) {
-		props.onChange = (e) => {
-			var val = e.target.value;
-			clearTimeout(timeout);
-			timeout = setTimeout(() => {
-				props.$value.value = val;
-			}, 100);
-		}
-		props.defaultValue = props.$value.value || '';
-	}
+const Input = ({...props}) => {
+    if (props.$value) {
+        props.onChange = (e) => {
+            var val = e.target.value;
+            clearTimeout(timeout);
+            timeout = setTimeout(() => {
+                props.$value.value = val;
+            }, 100);
+        }
+        props.defaultValue = props.$value.value || '';
+    }
 
-	return <DefaultOutlinedInput fullWidth={props.fullWidth || true} labelWidth={props.labelWidth || 0} {..._.omit(props, ['$value', 'classes'])} />
+    return <DefaultOutlinedInput fullWidth={props.fullWidth || true}
+                                 labelWidth={props.labelWidth || 0} {..._.omit(props, ['$value', 'classes'])} />
 }
 
 export default withTheme(Input);
