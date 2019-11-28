0117a7f7d2e78db637b832e930d2269338065b11
andre bastos <andre.bastos@geratriz.pt>
2019-11-28 23:06:27 +0000

    Update GSelect react component

--- Hydra.Such.Portal/Hydra.Such.Portal/ReactApp/components/molecules/GSelect/index.js
+++ Hydra.Such.Portal/Hydra.Such.Portal/ReactApp/components/molecules/GSelect/index.js
@@ -1,6 +1,6 @@
 import React from 'react';
-import styled, { css, theme } from 'styled-components';
-import { Input, Button, Icon, Text, Wrapper, Select, MenuItem } from 'components';
+import styled, {css, theme} from 'styled-components';
+import {Input, Button, Icon, Text, Wrapper, Select, MenuItem} from 'components';
 
 const Span = styled.span` 
     color: ${props => props.theme.palette.primary.medium};
@@ -8,21 +8,21 @@ const Span = styled.span`
 
 const GSelect = (props) => {
 
-      return (
+    return (
         <div>
-          <Select value >
-          {props.nullable&&
-            <MenuItem value><em>None</em></MenuItem>
-          }
-          {props.placeholder&&
-            <MenuItem value style={{display: 'none'}}><Span>{props.placeholder}</Span></MenuItem>
-          }
-              {props.options&&props.options.map(item => {
-                return <MenuItem value={item.value}>{item.title}</MenuItem>
-              } )}
-          </Select> 
+            <Select value>
+                {props.nullable &&
+                <MenuItem value><em>None</em></MenuItem>
+                }
+                {props.placeholder &&
+                <MenuItem value style={{display: 'none'}}><Span>{props.placeholder}</Span></MenuItem>
+                }
+                {props.options && props.options.map((item, i) => {
+                    return <MenuItem value={item.value} key={i}>{item.title}</MenuItem>
+                })}
+            </Select>
         </div>
     )
-  }
+}
 
 export default GSelect;
