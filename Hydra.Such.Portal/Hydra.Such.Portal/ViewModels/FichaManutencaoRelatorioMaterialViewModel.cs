fb34f99f31e81584ae1255d9bbf2254687dd26d2
andre bastos <andre.bastos@geratriz.pt>
2019-11-28 23:10:35 +0000

    Adds FichaMAnutencaoRelatorioMaterialViewModel

---
+++ Hydra.Such.Portal/Hydra.Such.Portal/ViewModels/FichaManutencaoRelatorioMaterialViewModel.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.AspNet.OData.Query;
+using Microsoft.AspNetCore.Mvc;
+using Newtonsoft.Json;
+using Newtonsoft.Json.Converters;
+using NJsonSchema;
+using NJsonSchema.Annotations;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Runtime.Serialization;
+
+namespace Hydra.Such.Portal.ViewModels
+{
+
+    public class FichaManutencaoRelatorioMaterialViewModel
+    {
+        public int? Id { get; set; }
+        public string Descricao { get; set; }
+        public string Quantidade { get; set; }
+        public int? FornecidoPor { get; set; }
+    }
+}
