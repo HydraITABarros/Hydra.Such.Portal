4c08b1848ba90772ef7cf27443a243e7181ba364
andre bastos <andre.bastos@geratriz.pt>
2020-02-21 14:29:45 +0000

    Update React App index.js

--- Hydra.Such.Portal/Hydra.Such.Portal/ReactApp/index.js
+++ Hydra.Such.Portal/Hydra.Such.Portal/ReactApp/index.js
@@ -1,14 +1,14 @@
 // https://github.com/diegohaz/arc/wiki/Example-app
 import 'react-hot-loader/patch'
 import React from 'react'
-import { render } from 'react-dom'
-import { BrowserRouter } from 'react-router-dom'
+import {render} from 'react-dom'
+import {BrowserRouter} from 'react-router-dom'
 
-import { basename } from 'config'
+import {basename} from 'config'
 import App from 'App'
 
-import { useStrict } from 'mobx';
-import { Provider } from 'mobx-react';
+import {useStrict} from 'mobx';
+import {Provider} from 'mobx-react';
 
 // For easier debugging
 // window._____APP_STATE_____ = stores;
@@ -16,17 +16,17 @@ import { Provider } from 'mobx-react';
 //useStrict(true);
 
 const renderApp = () => (
-	<BrowserRouter basename={basename}>
-		<App />
-	</BrowserRouter>
-)
+    <BrowserRouter basename={basename}>
+        <App/>
+    </BrowserRouter>
+);
 
 const root = document.getElementById('basicreactcomponent')
-render(renderApp(), root)
+render(renderApp(), root);
 
 if (module.hot) {
-	module.hot.accept('App', () => {
-		require('App')
-		render(renderApp(), root)
-	})
+    module.hot.accept('App', () => {
+        require('App');
+        render(renderApp(), root)
+    })
 }
