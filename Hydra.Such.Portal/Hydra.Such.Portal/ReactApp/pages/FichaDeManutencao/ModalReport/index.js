6331d002bff3bb338a636a01acf7d3c94a991441
andre bastos <andre.bastos@geratriz.pt>
2020-02-21 14:31:15 +0000

    update react ficha de manutencao final report

--- Hydra.Such.Portal/Hydra.Such.Portal/ReactApp/pages/FichaDeManutencao/ModalReport/index.js
+++ Hydra.Such.Portal/Hydra.Such.Portal/ReactApp/pages/FichaDeManutencao/ModalReport/index.js
@@ -5,6 +5,7 @@ import {
     Icon,
     CheckBox,
     ModalLarge,
+    Radio
 } from 'components';
 import {Grid} from '@material-ui/core';
 import Report from './Report';
@@ -21,11 +22,14 @@ class ModalReport extends Component {
     state = {
         open: false,
         reportMode: true,
-        selectedEquipments: []
-    }
+        selectedEquipments: [],
+        isGrouped: false,
+        reportList: []
+    };
 
     constructor(props) {
         super(props);
+        this.setReportGroup = this.setReportGroup.bind(this);
     }
 
     componentDidMount() {
@@ -37,6 +41,7 @@ class ModalReport extends Component {
 
     componentWillUnmount() {
         this.state.selectedEquipments = [];
+        this.state.reportList = [];
     }
 
     componentWillReceiveProps(nextProps) {
@@ -50,75 +55,102 @@ class ModalReport extends Component {
             var selectedEquipments = [];
             if (this.props.$equipments) {
                 this.props.$equipments.value.map((item, i) => {
-                    if (i > 4) {
-                        return;
-                    }
+                    // if (i > 4) {
+                    //     return;
+                    // }
                     item.checked = true;
                     selectedEquipments.push(item);
-                })
-                this.setState({
-                    selectedEquipments
+                    this.setReportGroup(selectedEquipments);
                 });
+
+
             }
         }
+    }
 
+    setReportGroup(selectedEquipments) {
+        let reportList = [];
+        if (this.state.isGrouped) {
+            reportList = _.chunk(selectedEquipments, 5)
+        } else {
+            reportList = _.chunk(selectedEquipments, 1)
+        }
+
+        this.setState({
+            reportList,
+            selectedEquipments,
+
+        });
     }
 
     makePDF(pageTotal, fileName) {
-        var quotes = document.getElementById('report');
-        quotes.parentNode.scrollTop = 0;
-        pageTotal = pageTotal - 1;
-        html2canvas(quotes, {
-            scale: 1,
-            dpi: 300
-        }).then((canvas) => {
-            //! MAKE YOUR PDF
-            var pdf = new jsPDF('p', 'pt', 'a4');
+        //var quotes = document.getElementById('report');
+        var quotes_list = $('.report-js');
+        window.onePageCanvas = [];
+        $('.report__blank').hide();
+        quotes_list.map((index, quotes) => {
+            console.log(quotes, index);
+            quotes = quotes;
+            // if (index == 0) {
+            //     $(quotes).parents('.report__container')[0].scrollTop = 0;
+            // } else {
+            //     $(quotes).parents('.report__container')[0].scrollTop = $($('.report__wrapper__root')[index - 1]).height() + 50;
+            // }
 
-            for (var i = 0; i <= pageTotal; i++) {
-                //! This is all just html2canvas stuff
-                var srcImg = canvas;
-                var sX = 0;
-                var sY = 1123 * i; // start 980 pixels down for every new page
-                var sWidth = 794;
-                var sHeight = 1123;
-                var dX = 0;
-                var dY = 0;
-                var dWidth = 794;
-                var dHeight = 1123;
+            pageTotal = $(quotes).find('.report__page').length - 1;
+            html2canvas(quotes, {
+                scale: 1,
+                dpi: 350
+            }).then((canvas) => {
+                //! MAKE YOUR PDF
+                var pdf = new jsPDF('p', 'pt', 'a4');
 
-                window.onePageCanvas = document.createElement("canvas");
-                //resolution
-                onePageCanvas.setAttribute('width', 794);
-                onePageCanvas.setAttribute('height', 1123);
-                var ctx = onePageCanvas.getContext('2d');
-                // details on this usage of this function:
-                // https://developer.mozilla.org/en-US/docs/Web/API/Canvas_API/Tutorial/Using_images#Slicing
-                ctx.drawImage(srcImg, sX, sY, sWidth, sHeight, dX, dY, dWidth, dHeight);
+                for (var i = 0; i <= pageTotal; i++) {
+                    //! This is all just html2canvas stuff
+                    var srcImg = canvas;
+                    var sX = 0;
+                    var sY = 1123 * i; // start 980 pixels down for every new page
+                    var sWidth = 794;
+                    var sHeight = 1123;
+                    var dX = 0;
+                    var dY = 0;
+                    var dWidth = 794;
+                    var dHeight = 1123;
 
-                // document.body.appendChild(canvas);
-                var canvasDataURL = onePageCanvas.toDataURL("image/png", .5);
+                    window.onePageCanvas[index] = document.createElement("canvas");
+                    //resolution
+                    onePageCanvas[index].setAttribute('width', 794);
+                    onePageCanvas[index].setAttribute('height', 1123);
+                    var ctx = onePageCanvas[index].getContext('2d');
+                    // details on this usage of this function:
+                    // https://developer.mozilla.org/en-US/docs/Web/API/Canvas_API/Tutorial/Using_images#Slicing
+                    ctx.drawImage(srcImg, sX, sY, sWidth, sHeight, dX, dY, dWidth, dHeight);
 
-                var width = onePageCanvas.width;
-                var height = onePageCanvas.clientHeight;
+                    // document.body.appendChild(canvas);
+                    var canvasDataURL = onePageCanvas[index].toDataURL("image/png", .5);
 
-                //! If we're on anything other than the first page,
-                // add another page
-                if (i > 0) {
-                    //pdf.addPage(612, 791); //8.5" x 11" in pts (in*72)
-                    pdf.addPage(595, 842); //8.5" x 11" in pts (in*72)
-                    //pdf.addPage(794, 1123); //8.5" x 11" in pts (in*72)
-                    //pdf.addPage(1240, 1754); //8.5" x 11" in pts (in*72)
+                    var width = onePageCanvas[index].width;
+                    var height = onePageCanvas[index].clientHeight;
+
+                    //! If we're on anything other than the first page,
+                    // add another page
+                    if (i > 0) {
+                        //pdf.addPage(612, 791); //8.5" x 11" in pts (in*72)
+                        pdf.addPage(595, 842); //8.5" x 11" in pts (in*72)
+                        //pdf.addPage(794, 1123); //8.5" x 11" in pts (in*72)
+                        //pdf.addPage(1240, 1754); //8.5" x 11" in pts (in*72)
+                    }
+                    //! now we declare that we're working on that page
+                    pdf.setPage(i + 1);
+                    //! now we add content to that page!
+                    pdf.addImage(canvasDataURL, 'PNG', 0, 0, (width * .75), (height * .75), null, 'SLOW');
+
                 }
-                //! now we declare that we're working on that page
-                pdf.setPage(i + 1);
-                //! now we add content to that page!
-                pdf.addImage(canvasDataURL, 'PNG', 0, 0, (width * .75), (height * .75), null, 'SLOW');
-
-            }
-            //! after the for loop is finished running, we save the pdf.
-            pdf.output('dataurlnewwindow');
-            //pdf.save(fileName + '.pdf' || 'File.pdf');
+                //! after the for loop is finished running, we save the pdf.
+                pdf.output('dataurlnewwindow');
+                $('.report__blank').show();
+                //pdf.save(fileName + '.pdf' || 'File.pdf');
+            });
         });
     }
 
@@ -151,10 +183,11 @@ class ModalReport extends Component {
                     </DialogTitle>
                     <hr/>
 
-                    <Grid container direction="row" justify="space-between" alignitems="top" spacing={0}
+                    <Grid container direction="row" justify="space-between" alignitems="bottom" spacing={0}
                           maxwidth={'100%'} margin={0}
                           className={"modal-container"}>
-                        <Grid item xs={12} md={3} lg={3} style={{borderBottom: '1px solid #E4E7EB'}}>
+                        <Grid item xs={12} md={3} lg={3}
+                              style={{borderBottom: '1px solid #E4E7EB', 'position': 'relative', marginBottom: '9px'}}>
                             <DialogContent>
                                 <div className="col-xs-10 p-l-0 p-r-0">
                                     <Text b className="ws-nowrap to-ellipsis">{this.props.equipmentType}</Text>
@@ -176,30 +209,57 @@ class ModalReport extends Component {
                                                               checked={equipment.checked}
                                                               onChange={(event) => {
 
-                                                                  setTimeout(() => {
-                                                                      this.setState({});
-                                                                  }, 10)
-
                                                                   if (event.target.checked) {
-                                                                      this.state.selectedEquipments = this.state.selectedEquipments.filter((selected) => {
+                                                                      var selectedEquipments = this.state.selectedEquipments.filter((selected) => {
                                                                           return selected.idEquipamento != equipment.idEquipamento;
                                                                       });
                                                                       equipment.checked = true;
-                                                                      this.state.selectedEquipments.push(equipment);
-                                                                      this.setState({selectedEquipments: this.state.selectedEquipments});
+                                                                      selectedEquipments.push(equipment);
                                                                   } else {
-                                                                      this.state.selectedEquipments = this.state.selectedEquipments.filter((selected) => {
+                                                                      var selectedEquipments = this.state.selectedEquipments.filter((selected) => {
                                                                           return selected.idEquipamento != equipment.idEquipamento;
                                                                       });
                                                                       equipment.checked = false;
-                                                                      this.setState({selectedEquipments: this.state.selectedEquipments || []});
                                                                   }
+
+                                                                  this.setState({
+                                                                      selectedEquipments: selectedEquipments,
+                                                                      reportList: []
+                                                                  }, () => {
+                                                                      setTimeout(() => {
+                                                                          this.setReportGroup(this.state.selectedEquipments);
+                                                                      }, 40)
+                                                                  })
                                                               }}/>
                                                 </div>
                                                 <div className="w-auto ws-nowrap to-ellipsis v-a-m">
                                                     <label htmlFor={"report-checkbox-" + i}>
                                                         <Text b className="w-20">#{i + 1}</Text> &nbsp;<Text
                                                         span>{equipment.numEquipamento}</Text>
+                                                    </label>
+                                                </div>
+
+
+                                                <div className={"modal-report__group"}>
+                                                    <CheckBox
+                                                        onChange={(ev) => {
+                                                            this.setState({
+                                                                isGrouped: ev.target.checked,
+                                                                reportList: []
+                                                            }, () => {
+                                                                setTimeout(() => {
+                                                                    this.setReportGroup(this.state.selectedEquipments);
+                                                                }, 4)
+                                                            })
+                                                        }}
+                                                        checked={this.state.isGrouped}
+                                                        name="radio-button-group"
+                                                        id="radio-button-group"
+                                                        label="Agrupar"
+                                                        className={""}
+                                                    />
+                                                    <label htmlFor={"radio-button-group"}>
+                                                        <Text span>Agrupar</Text>
                                                     </label>
                                                 </div>
                                             </div>
@@ -230,30 +290,39 @@ class ModalReport extends Component {
                                   textAlign: 'center'
                               }}
                               className={"report__container " + (this.state.selectedEquipments.length > 0 ? "" : "content-disabled")}>
-                            {this.state.reportMode ?
-                                <Report
-                                    selectedEquipments={this.state.selectedEquipments}
-                                    order={this.props.order}
-                                    equipmentType={this.props.equipmentType}
-                                    $equipments={this.props.$equipments}
+                            <div>
+                                {this.state.reportMode ?
+                                    this.state.reportList.map((selectedEquipments, i) => {
+                                        return (
+                                            <React.Fragment
+                                                key={i}>
+                                                <Report
+                                                    className={'report__container'}
+                                                    selectedEquipments={selectedEquipments}
+                                                    order={this.props.order}
+                                                    equipmentType={this.props.equipmentType}
+                                                    $equipments={this.props.$equipments}
 
-                                    isCurative={this.props.isCurative}
-                                    isSimplified={this.props.isSimplified}
+                                                    isCurative={this.props.isCurative}
+                                                    isSimplified={this.props.isSimplified}
 
-                                    onReportSplit={(totalPages) => {
-                                        this.state.totalPages = totalPages;
-                                    }}
-                                /> :
-                                <Signature $equipments={this.props.$equipments}
-                                           $currentUser={this.props.$currentUser}
-                                           onChange={() => {
-                                               if (this.props.onChange) {
-                                                   this.props.onChange();
-                                               }
-                                           }}
-                                />
-                            }
-
+                                                    onReportSplit={(totalPages) => {
+                                                        this.state.totalPages = totalPages;
+                                                    }}
+                                                />
+                                            </React.Fragment>
+                                        )
+                                    }) :
+                                    <Signature $equipments={this.props.$equipments}
+                                               $currentUser={this.props.$currentUser}
+                                               onChange={() => {
+                                                   if (this.props.onChange) {
+                                                       this.props.onChange();
+                                                   }
+                                               }}
+                                    />
+                                }
+                            </div>
                         </Grid>
                     </Grid>
 
