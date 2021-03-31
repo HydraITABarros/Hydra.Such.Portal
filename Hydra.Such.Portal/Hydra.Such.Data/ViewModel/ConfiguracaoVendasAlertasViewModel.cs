1dbff7756b5bf8ba8d58c3c67307083a5ec03327
ARomao@such.pt <ARomao@such.pt>
2021-03-31 14:01:26 +0100

    ARomao@such.pt

--- Hydra.Such.Portal/Hydra.Such.Data/ViewModel/ConfiguracaoVendasAlertasViewModel.cs
+++ Hydra.Such.Portal/Hydra.Such.Data/ViewModel/ConfiguracaoVendasAlertasViewModel.cs
@@ -11,15 +11,19 @@ namespace Hydra.Such.Data.ViewModel
         public string Email1Regiao12 { get; set; }
         public string Email2Regiao12 { get; set; }
         public string Email3Regiao12 { get; set; }
+        public string Email4Regiao12 { get; set; }
         public string Email1Regiao23 { get; set; }
         public string Email2Regiao23 { get; set; }
         public string Email3Regiao23 { get; set; }
+        public string Email4Regiao23 { get; set; }
         public string Email1Regiao33 { get; set; }
         public string Email2Regiao33 { get; set; }
         public string Email3Regiao33 { get; set; }
+        public string Email4Regiao33 { get; set; }
         public string Email1Regiao43 { get; set; }
         public string Email2Regiao43 { get; set; }
         public string Email3Regiao43 { get; set; }
+        public string Email4Regiao43 { get; set; }
         public int? DiasParaEnvioAlerta { get; set; }
         public int? DiasParaEnvioAlertaAudienciaPrevia { get; set; }
         public string UtilizadorCriacao { get; set; }
